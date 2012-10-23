c5dc8fd678b5cf10a22346ee44df7cd729f53bab
Marc Burns <m4burns@uwaterloo.ca>
2012-10-23 00:53:19 -0400

    Add some documentation.

--- src/seashell-api.rkt
+++ src/seashell-api.rkt
@@ -5,6 +5,28 @@
            "database.rkt")
   (provide start-api)
 
+  (define (handle-login req)
+    ;; More login logic goes here.
+    ;; XXX TODO security considerations
+    ;; If this point is reached, the user is considered to be authenticated
+    ;;  and authorized to use the program. So:
+    ;;    (1) Generate a session key.
+    ;;    (2) Store the session key with the Quest user ID in the database.
+    ;;      TODO Storage backing the db must be secure.
+    ;;      TODO Don't leak function arguments in stack traces.
+    ;;    (3) Respond with the session key. The client is responsible for
+    ;;        storing the session key.
+
+    ;; Once authenticated, the client begins a set of interactions with the server
+    ;; by exchanging the session key for a continuation table which defines
+    ;; the API provided by the server. This process is agnostic of client.
+
+    ;; The server may respond to an API call with an 'unknown continuation' message.
+    ;; In this case, the client must present the session key again in hopes of receiving
+    ;; a new continuation table. If no table is provided, then the session is considered
+    ;; terminated by the server.
+
+  ;; Some examples.
   (define (do-save req)
     ;; save the document here.
     (printf "do-save~n")
