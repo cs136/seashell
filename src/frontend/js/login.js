264918d8a82a4ad65c2aae43cec0df4f45eef4eb
Marc Burns <m4burns@csclub.uwaterloo.ca>
2013-09-18 15:33:47 -0400

    Fix up login form.

--- src/frontend/js/login.js
+++ src/frontend/js/login.js
@@ -22,16 +22,34 @@ function eraseCookie(name) {
     createCookie(name, "", -1);
 }
 
+function fixedEncodeURIComponent (str) {
+    return encodeURIComponent(str)
+            .replace(/[!'()]/g, escape)
+            .replace(/\*/g, "%2A")
+            .replace("%20", "+");
+}
+
+function init_login() {
+  $('#login-username')[0].disabled = false;
+  $('#login-password')[0].disabled = false;
+  $('#login-submit')  [0].disabled = false;
+  $('#login-username')[0].focus();
+}
+
 function submit_login() {
+  var user = $('#login-username')[0].value;
+  var pass = $('#login-password')[0].value;
+  var upload = "u=" + fixedEncodeURIComponent(user)
+               + "&p=" + fixedEncodeURIComponent(pass);
   $.post("login.cgi",
-         $("#loginform").serialize(),
+         upload,
          function(textdata, textStatus, jqXHR) {
            data = JSON.parse(textdata);
            console.log("Response: " + textdata);
            window.ssdata = data;
            if(typeof(data) == "object") {
              if(data.error !== undefined) {
-               alert("Login error: " + data.message + " (code " + data.code + ")");
+               alert("Login error: " + data.error.message + " (code " + data.error.code + ")");
              } else if(data.port !== undefined) {
                createCookie("seashell-session", JSON.stringify(data), 365);
                console.log("All done login.");
