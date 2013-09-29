2aa605d658c582638cb6853a091ffd330323f723
Edward Lee <e45lee@uwaterloo.ca>
2013-09-29 12:41:18 -0400

    More cleanup.

--- src/collects/seashell/compiler/seashell-compiler.rkt
+++ src/collects/seashell/compiler/seashell-compiler.rkt
@@ -38,62 +38,68 @@
 
   ;; Run the compiler + intermediate linkage step.
   (define compiler-res (seashell_compiler_run compiler))
+  ;; Grab the results of running the intermediate code generation step.
+  (define intermediate-linker-diags (seashell_compiler_get_linker_messages compiler))
+  ;; Generate our diagnostics:
   (define compiler-diags
     (foldl
       (lambda (message table)
-        (if (not (equal? "" (cdr message)))
+        (if (not (equal? "" (seashell-diagnostic-message (cdr message))))
           (hash-set table (car message)
-                    (cons (seashell-diagnostic "" 0 0 (cdr message))
+                    (cons (cdr message)
                           (hash-ref table (car message) '())))
           table))
       (make-immutable-hash)
-      (for*/list ([i (in-range 0 (length sources))]
-                  [j (in-range 0 (seashell_compiler_get_diagnostic_count compiler i))])
-        `(,(vector-ref file-vec i) .
-            ,(seashell-diagnostic (seashell_compiler_get_diagnostic_file compiler i j)
-                                  (seashell_compiler_get_diagnostic_line compiler i j)
-                                  (seashell_compiler_get_diagnostic_column compiler i j)
-                                  (seashell_compiler_get_diagnostic_message compiler i j))))))
-  ;; Grab the results of running the intermediate code generation step.
-  (define intermediate-linker-diags (seashell_compiler_get_linker_messages compiler))
-
-  ;; Invoke the final link step on the object file.
-  (define object (seashell_compiler_get_object compiler))
-  ;; Write it to a temporary file, invoke cc [-Wl,$ldflags] -o /dev/stdout <temporary_file>
-  (define temporary-file (make-temporary-file "seashell-object-~a"))
-  (with-output-to-file temporary-file
-                       (thunk (write-bytes object))
-                       #:exists 'truncate)
-  (define-values (linker linker-output linker-input linker-error)
-    (apply subprocess #f #f #f "/usr/bin/cc" 
-           (append (map (curry string-append "-Wl,") ldflags)
-                   (list "-o" "/dev/stdout" (path->string temporary-file)))))
-  ;; Close unused port.
-  (close-output-port linker-input)
-  ;; Read result from linker:
-  (define linker-result (port->bytes linker-output))
-  (define linker-messages (port->string linker-error))
-  (close-input-port linker-output)
-  (close-input-port linker-error)
-  ;; Get the linker termination code
-  (define linker-res (subprocess-status (sync linker)))
-
-  ;; Create the final diagnostics table:
-  (define diags
-    (foldl
-      (lambda (message table)
-        (if (not (equal? "" (cdr message)))
-          (hash-set table (car message)
-                    (cons (seashell-diagnostic "" 0 0 (cdr message))
-                          (hash-ref table (car message) '())))
-          table))
-      compiler-diags
-      ;; Create list of linker diagnostics:
       (list*
-        (cons (string->path "intermediate-link-result") intermediate-linker-diags)
-        (map (curry cons (string->path "final-link-result"))
-             (string-split linker-messages #px"\n")))))
+        `(,(string->path "intermediate-link-result") . ,(seashell-diagnostic "" 0 0 intermediate-linker-diags))
+        (for*/list ([i (in-range 0 (length sources))]
+                    [j (in-range 0 (seashell_compiler_get_diagnostic_count compiler i))])
+          `(,(vector-ref file-vec i) .
+             ,(seashell-diagnostic (seashell_compiler_get_diagnostic_file compiler i j)
+                                   (seashell_compiler_get_diagnostic_line compiler i j)
+                                   (seashell_compiler_get_diagnostic_column compiler i j)
+                                   (seashell_compiler_get_diagnostic_message compiler i j)))))))
+
+  (cond
+    [(zero? compiler-res)
+      ;; Invoke the final link step on the object file - if it exists.
+      (define object (seashell_compiler_get_object compiler))
+      ;; Write it to a temporary file, invoke cc [-Wl,$ldflags] -o /dev/stdout <temporary_file>
+      (define temporary-file (make-temporary-file "seashell-object-~a"))
+      (with-output-to-file temporary-file
+                           (thunk (write-bytes object))
+                           #:exists 'truncate)
+      (define-values (linker linker-output linker-input linker-error)
+        (apply subprocess #f #f #f "/usr/bin/cc" 
+               (append (map (curry string-append "-Wl,") ldflags)
+                       (list "-o" "/dev/stdout" (path->string temporary-file)))))
+      ;; Close unused port.
+      (close-output-port linker-input)
+      ;; Read result from linker:
+      (define linker-result (port->bytes linker-output))
+      (define linker-messages (port->string linker-error))
+      (close-input-port linker-output)
+      (close-input-port linker-error)
+      ;; Get the linker termination code
+      (define linker-res (subprocess-status (sync linker)))
+
+      ;; Create the final diagnostics table:
+      (define diags
+        (foldl
+          (lambda (message table)
+            (if (not (equal? "" (cdr message)))
+              (hash-set table (car message)
+                        (cons (seashell-diagnostic "" 0 0 (cdr message))
+                              (hash-ref table (car message) '())))
+              table))
+          compiler-diags
+          ;; Create list of linker diagnostics:
+          (list*
+            (map (curry cons (string->path "final-link-result"))
+                 (string-split linker-messages #px"\n")))))
   
-  (if (and (zero? compiler-res) (zero? linker-res))
-    (values linker-result diags)
-    (values #f diags)))
+      (if (and (zero? compiler-res) (zero? linker-res))
+        (values linker-result diags)
+        (values #f diags))]
+    [else
+      (values #f compiler-diags)]))
