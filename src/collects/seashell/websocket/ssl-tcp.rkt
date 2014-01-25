0fde674055a7d4b3d440138cde49a2205786a99f
Edward Lee <e45lee@uwaterloo.ca>
2014-01-25 11:34:23 -0500

    [ssl] Added revised SSL TCP unit.

---
+++ src/collects/seashell/websocket/ssl-tcp.rkt
@@ -0,0 +1,96 @@
+#lang racket/base
+;; Seashell's websocket library.
+;; Copyright (C) 2013-2014 The Seashell Maintainers.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file was modified from ssl-unit-tcp.rkt in Racket 5.3.6
+(provide make-ssl-tcp@)
+(require racket/unit net/tcp-sig openssl/mzssl
+         (prefix-in tcp: racket/tcp))
+
+(define (make-ssl-tcp@
+         server-cert-file server-key-file server-root-cert-files
+         server-suggest-auth-file
+         client-cert-file client-key-file client-root-cert-files)
+  (unit
+   (import)
+   (export tcp^)
+
+   (define ctx (ssl-make-client-context))
+   (define sctx (ssl-make-server-context))
+
+   (when server-cert-file
+     (ssl-load-certificate-chain! sctx server-cert-file))
+   (when server-key-file
+     (ssl-load-private-key! sctx server-key-file))
+   (when server-root-cert-files
+     (ssl-set-verify! sctx #t)
+     (map (lambda (f)
+            (ssl-load-verify-root-certificates! sctx f))
+          server-root-cert-files))
+   (when server-suggest-auth-file
+     (ssl-load-suggested-certificate-authorities! sctx server-suggest-auth-file))
+
+   (when client-cert-file
+     (ssl-load-certificate-chain! ctx client-cert-file))
+   (when client-key-file
+     (ssl-load-private-key! ctx client-key-file))
+   (when client-root-cert-files
+     (ssl-set-verify! ctx #t)
+     (map (lambda (f)
+            (ssl-load-verify-root-certificates! ctx f))
+          client-root-cert-files))
+
+   (define (tcp-abandon-port p)
+     (if (input-port? p)
+       (close-input-port p)
+       (close-output-port p)))
+
+   (define (tcp-accept listener)
+     (define-values (ip op) (tcp:tcp-accept listener))
+     (ports->ssl-ports ip op
+                       #:mode 'accept
+                       #:context sctx
+                       #:close-original? #t))
+
+   (define (tcp-accept/enable-break listener)
+     (define-values (ip op) (tcp:tcp-accept/enable-break listener))
+     (ports->ssl-ports ip op
+                       #:mode 'accept
+                       #:context sctx
+                       #:close-original? #t))
+
+   ;; accept-ready? doesn't really work for SSL:
+   (define (tcp-accept-ready? p)
+     #f)
+
+   (define (tcp-addresses port [port-numbers? #f])
+     (cond
+       [(tcp:tcp-listener? port)
+        (tcp:tcp-addresses port port-numbers?)]
+       [else
+        (ssl-addresses port port-numbers?)]))
+
+   (define tcp-close ssl-close)
+   (define (tcp-connect hostname port-k)
+     (ssl-connect hostname port-k ctx))
+   (define (tcp-connect/enable-break hostname port-k)
+     (ssl-connect/enable-break hostname port-k ctx))
+
+   (define tcp-listen tcp:tcp-listen)
+
+   (define tcp-listener? ssl-listener?)))
