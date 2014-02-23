fb569ae8206f2cec079ecab2f5c090f8df7c4b6e
Edward Lee <e45lee@uwaterloo.ca>
2014-02-22 21:12:50 -0500

    [frontend] Set secure on cookie.

--- src/frontend/js/login/login.js
+++ src/frontend/js/login/login.js
@@ -32,7 +32,7 @@ function createCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         var expires = "; expires=" + date.toGMTString();
     } else var expires = "";
-    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
+    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/; secure";
 }
 
 /**
