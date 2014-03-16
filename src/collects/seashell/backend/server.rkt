463bdc4095c76e16260a87b534d849b9c13cdb9c
Edward Lee <e45lee@uwaterloo.ca>
2014-03-15 21:25:56 -0400

    [backend] Modify backend-main to reliably write the credentials file.

--- src/collects/seashell/backend/server.rkt
+++ src/collects/seashell/backend/server.rkt
@@ -35,7 +35,6 @@
          web-server/dispatchers/dispatch
          (prefix-in sequence: web-server/dispatchers/dispatch-sequencer)
          (prefix-in filter: web-server/dispatchers/dispatch-filter))
-         
 
 (provide backend-main)
 
@@ -48,6 +47,14 @@
   (-> void?)
   (when (not (directory-exists? (read-config 'seashell)))
     (make-directory (read-config 'seashell))))
+
+;; exit-from-seashell return -> any/c
+;; Seashell-specific exit function.
+;;
+;; Currently waits a few seconds (to deal with the log not flushing properly)
+(define (exit-from-seashell return)
+  (sleep 3)
+  (exit return))
 
 ;; (backend/main)
 ;; Entry point to the backend server.
@@ -65,90 +72,141 @@
   ;; Dropping permissions.
   (unless (= 0 (seashell_drop_permissions))
     (fprintf (current-error-port) "Failed to drop permissions!  Exiting...~n")
-    (exit 1))
+    (exit-from-seashell 1))
   
   (with-handlers
-    ([exn? (lambda (exn)
-             (fprintf (current-error-port) "Exception raised in startup code: ~a~n" (exn-message exn))
-             (exit 1))])
-
+      ([exn:fail? 
+        (lambda (exn)
+          (fprintf (current-error-port) "Exception raised in startup code: ~a~n" (exn-message exn))
+          (exit-from-seashell 2))])
+    
     ;; Directory setup.
     (init-environment)
     (init-projects)
-
-    ;; Log / handlers setup.
-    (current-error-port (open-output-file (build-path (read-config 'seashell) "seashell.log")
-                                          #:exists 'append))
-    (standard-logger-setup))
-
-  (with-handlers
-    ([exn? (lambda (exn)
-             (logf 'error "Exception raised in Seashell!  Exiting...")
-             ;; TODO: cleanup code
-             (exit 1))])
-  
-    (logf 'info "Starting up.")
     
-    ;; Unbuffered mode for I/O ports
-    (file-stream-buffer-mode (current-input-port) 'none)
+    ;; Replace stderr with a new port that writes to a log file in the user's Seashell directory.
+    ;; (define old-error-port (current-error-port))
+    ;; (current-error-port (open-output-file (build-path (read-config 'seashell) "seashell.log")
+    ;;                                      #:exists 'append))
+    ;; (close-output-port old-error-port)
+    
+    ;; Unbuffered mode for output ports.
     (file-stream-buffer-mode (current-output-port) 'none)
     (file-stream-buffer-mode (current-error-port) 'none)
-
-    ;; If another instance of the server is running, send credentials for that instance and exit
-    (when (file-exists? (build-path (read-config 'seashell) "creds"))
-        (logf 'info "Found existing seashell instance lock file; using existing credentials.")
-        (with-input-from-file (build-path (read-config 'seashell) "creds") (thunk
-          (write (read))))
-        (exit 0))
     
-    ;; Global dispatcher.
-    (define seashell-dispatch
-      (sequence:make
-        request-logging-dispatcher
-        (filter:make #rx"^/$" (make-websocket-dispatcher 
-                               (curry conn-dispatch keepalive-chan)))
-        (filter:make #rx"^/export/" project-export-dispatcher)
-        (filter:make #rx"^/upload$" upload-file-dispatcher)
-        standard-error-dispatcher))
-
-    ;; Start the server.
-    (define conf-chan  (make-async-channel))
-    (define shutdown-server
-      (serve
-       #:dispatch seashell-dispatch
-       #:port 0
-       #:tcp@ ssl-unit
-       #:listen-ip "localhost"
-       #:confirmation-channel conf-chan))
-    (define start-result (async-channel-get conf-chan))
-    (when (exn? start-result)
-      (raise start-result))
-
-    ;; Generate and send credentials, write lock file
-    (define host (read))
-    (logf 'debug "Read hostname '~a' from login server" host)
-    (define key (seashell-crypt-make-key))
-    (install-server-key! key)
-    (define creds `#hash((key . ,key) (host . ,host) (port . ,start-result)))
-    (write (serialize creds))
-    (with-output-to-file (build-path (read-config 'seashell) "creds") (thunk (write (serialize creds))))
+    ;; Logging setup.
+    (standard-logger-setup))
+  
+  (with-handlers
+      ([exn:fail? 
+        (lambda (exn)
+          (logf 'error "Exception raised in Seashell: ~a~n" (exn-message exn))
+          (exit-from-seashell 3))])
     
-    ;; Write out the listening port
-    (logf 'info "Listening on port ~a." start-result)
-
-    ;; Loop and serve requests.
-    (with-handlers
-        ([exn:break? (lambda(e) (logf 'error "Terminating on break."))])
-      (let loop ()
-        (match (sync/timeout/enable-break (/ (read-config 'backend-client-idle-timeout) 1000) keepalive-chan)
-          [#f (void)]
-          [else (loop)])))
-
-    ;; Delete lock file
-    (delete-file (build-path (read-config 'seashell) "creds"))
+    (logf 'info "Starting up.")
     
-    ;; Shutdown.
-    (logf 'info "Shutting down...")
-    (shutdown-server)
-    (logf 'info "Graceful shutdown.")
-    (exit 0)))
+    ;; If another instance of the server is running, send credentials for that instance and exit-from-seashell
+    (define credentials-file (build-path (read-config 'seashell) "creds"))
+    (define credentials-port #f)
+    (define shutdown-server void)
+    
+    ;; Note: (exit-from-seashell ...) does not unwind the continuation stack.
+    (dynamic-wind
+     void
+     (thunk
+      (call-with-continuation-barrier
+       (thunk
+        ;; Install credentials or quit.
+        (set! credentials-port
+              (let loop ([tries 0])
+                (when (> tries 5)
+                  (logf 'info "Error opening credentials file - aborting!")
+                  (exit-from-seashell 4))
+                (define repeat (make-continuation-prompt-tag))
+                (call-with-continuation-prompt
+                 (thunk
+                  (with-handlers
+                      ([exn:fail:filesystem? (lambda (exn) (void))])
+                    (with-input-from-file
+                        credentials-file
+                      (thunk
+                       (define result (read))
+                       (if (eof-object? result)
+                           (begin
+                             (sleep 1)
+                             (abort-current-continuation repeat))
+                           (write result))))
+                    (logf 'info "Found existing Seashell instance; using existing credentials.")
+                    (exit-from-seashell 0))
+                  (with-handlers
+                      ([exn:fail:filesystem? (lambda (exn) 
+                                               (abort-current-continuation repeat))])
+                    (open-output-file credentials-file)))
+                 repeat
+                 (thunk (loop (add1 tries))))))
+        ;; Global dispatcher.
+        (define seashell-dispatch
+          (sequence:make
+           request-logging-dispatcher
+           (filter:make #rx"^/$" (make-websocket-dispatcher 
+                                  (curry conn-dispatch keepalive-chan)))
+           (filter:make #rx"^/export/" project-export-dispatcher)
+           (filter:make #rx"^/upload$" upload-file-dispatcher)
+           standard-error-dispatcher))
+        
+        ;; Start the server.
+        (define conf-chan  (make-async-channel))
+        (set! shutdown-server
+              (serve
+               #:dispatch seashell-dispatch
+               #:port 0
+               #:tcp@ ssl-unit
+               #:listen-ip "localhost"
+               #:confirmation-channel conf-chan))
+        (define start-result (async-channel-get conf-chan))
+        (when (exn? start-result)
+          (raise start-result))
+        
+        ;; Generate and send credentials, write lock file
+        (define host (read))
+        (logf 'debug "Read hostname '~a' from login server" host)
+        (define key (seashell-crypt-make-key))
+        (install-server-key! key)
+        (define creds `#hash((key . ,key) (host . ,host) (port . ,start-result)))
+        
+        ;; Write credentials back to file and to tunnel.
+        (write (serialize creds))
+        (write (serialize creds) credentials-port)
+        
+        ;; Close ports to parent...
+        (define old-output-port (current-output-port))
+        (define old-input-port (current-input-port))
+        (current-output-port (open-output-nowhere))
+        (close-output-port old-output-port)
+        (close-input-port old-input-port)
+        ;; Close the credentials port
+        (close-output-port credentials-port)
+        
+        ;; Write out the listening port
+        (logf 'info "Listening on port ~a." start-result)
+        
+        ;; Loop and serve requests.
+        (with-handlers
+            ([exn:break? (lambda(e) (logf 'info "Terminating on break."))])
+          (let loop ()
+            (match (sync/timeout/enable-break (/ (read-config 'backend-client-idle-timeout) 1000) keepalive-chan)
+              [#f (void)]
+              [else (loop)]))))))
+     (thunk
+      (logf 'info "Shutting down...")
+      ;; Close port (if not closed)
+      (unless (or (not credentials-port)
+                  (port-closed? credentials-port))
+        (close-output-port credentials-port))
+      ;; Shutdown server
+      (shutdown-server)
+      ;; Delete lock file
+      (delete-file credentials-file)))
+    
+    (logf 'info "Graceful shutdown.") 
+    (exit-from-seashell 0)))
