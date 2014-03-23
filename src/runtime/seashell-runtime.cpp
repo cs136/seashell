f7b1ffc54a5dd08ed38cf4004ee11534249f0482
Edward Lee <e45lee@uwaterloo.ca>
2014-03-23 11:25:21 -0400

    [runtime] Add library for setting buffering mode.

---
+++ src/runtime/seashell-runtime.cpp
@@ -0,0 +1,29 @@
+/**
+ * Seashell's user runtime support libraries.
+ * Copyright (C) 2014 The Seashell Maintainers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+namespace {
+  class IOBufferSetup {
+    public:
+      IOBufferSetup() {
+        setvbuf(stdout, NULL, _IONBF, 0);
+        setvbuf(stderr, NULL, _IONBF, 0);
+      }
+  } iosetup;
+}
