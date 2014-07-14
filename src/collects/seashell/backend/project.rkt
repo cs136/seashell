29d69470489972209c30371ce4fba06de9ea251b
Kaleb Alway <kpalway@uwaterloo.ca>
2014-07-14 13:19:25 -0400

    [testing] create "tests" directory on new project

--- src/collects/seashell/backend/project.rkt
+++ src/collects/seashell/backend/project.rkt
@@ -165,7 +165,8 @@
          (raise (exn:project
                   (format "Project already exists, or some other filesystem error occurred: ~a" (exn-message exn))
                   (current-continuation-marks))))])
-    (seashell-git-init (build-project-path name)))
+    (seashell-git-init (build-project-path name))
+    (make-directory (check-and-build-path (build-project-path name) "tests")))
   (void))
 
 ;; (new-project-from name source)
