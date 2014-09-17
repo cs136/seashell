7777d223c6660f3d730b85fb8ce561774e99806c
Kaleb Alway <kpalway@uwaterloo.ca>
2014-09-17 09:03:56 -0400

    Fix bug occurring when no marmoset submissions exist

--- src/frontend/js/model/project.js
+++ src/frontend/js/model/project.js
@@ -125,7 +125,7 @@ SeashellProject.prototype.getMarmosetResults = function(marm_project) {
         console.log("MARM: Submission at "+data[i][2]+": passed "+data[i][0]+" of "+data[i][1]+".");
         //marm_tag.append("<tr><td>"+data[i][0]+"</td><td>"+data[i][1]+"</td><td>"+data[i][2]+"</td></tr>");
       }
-      def.resolve(!isNaN(data[0][0]));
+      def.resolve((data[0] != undefined) && !isNaN(data[0][0]));
   }).fail(function() {
     def.reject();
   });
