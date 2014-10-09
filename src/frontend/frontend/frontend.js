42d95eed6fcd4eee54151bd65c18dc67fb664786
Edward Lee <e45lee@uwaterloo.ca>
2014-10-09 11:40:19 -0400

    [frontend] Websocket adjustments.

--- src/frontend/frontend/frontend.js
+++ src/frontend/frontend/frontend.js
@@ -1,4 +1,3 @@
-
 angular.module('frontend-app')
   // controller for the list of projects
   .controller('ProjectList', ['$scope',
@@ -11,15 +10,28 @@ angular.module('frontend-app')
   // websocket service
   .service('socket', ['$q', '$window', '$interval', 'cookieStore', function($q, $window, $interval, cookie) {
     var self = this;
+    self.s = null;
+
     var dccount = 0;
     var disconnect_callbacks = [];
     var reconnect_callbacks = [];
+    var creds = cookie.get("creds");
 
     self.register_disconnect_callback = function(cb) {
       disconnect_callbacks.push(cb);
-    }
+    };
     self.register_reconnect_callback = function(cb) {
       reconnect_callbacks.push(cb);
+    };
+
+    function connectSocket() {
+      self.s = new SeashellWebsocket("wss://" + creds.host + ":" + creds.port, creds.key);
+      return $q.when(self.s.ready)
+        .then(function () {
+          console.log("Seashell socket set up properly.");
+          $interval(setupDisconnectMonitor, 4000);
+          console.log("Websocket disconnection monitor set up properly.");
+        });
     }
 
     function setupDisconnectMonitor() {
@@ -34,29 +46,22 @@ angular.module('frontend-app')
         _.each(disconnect_callbacks, call);
       }
       if(self.s.websocket.readyState == 3) { // if socket is closed
-        self.s = new SeashellWebsocket("wss://" + creds.host + ":" + creds.port, creds.key);
-        $q.when(self.s.ready).done(onReconnect);
+        $q.when(self.s.ready).then(onReconnect);
       }
       else {
-        $q.when(self.s.ping).done(onReconnect);
+        $q.when(self.s.ping).then(onReconnect);
       }
     }
 
     SeashellCoder.addEntropy();
-    var creds = cookie.get("creds");
     if(creds) {
       self.s = new SeashellWebsocket("wss://" + creds.host + ":" + creds.port, creds.key);
       var qprom = $q.when(self.s.ready);
-      qprom.done(function() {
-        console.log("Seashell socket set up properly.");
-        $interval(setupDisconnectMonitor, 4000);
-        console.log("Websocket disconnection monitor set up properly.");
-      });
-      qprom.fail(function() {
+      qprom.catch(function() {
         displayErrorMessage("Seashell socket could not be set up.");
       });
     }
     else {
       $window.location.replace("/seashell/");
     }
-  });
+  }]);
