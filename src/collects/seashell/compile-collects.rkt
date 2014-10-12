d2785e3feafc2bd57db191782ffdb431eb77655a
Edward Lee <e45lee@uwaterloo.ca>
2014-10-12 11:12:25 -0400

    [backend] Use parallel-compile-files instead.

--- src/collects/seashell/compile-collects.rkt
+++ src/collects/seashell/compile-collects.rkt
@@ -1,10 +1,21 @@
 #lang racket
-(require compiler/cm
+(require setup/parallel-build
          seashell/seashell-config)
 
-(sequence-for-each
-  (lambda(path)
-    (when (and (file-exists? path) (equal? (filename-extension path) #"rkt"))
-      (printf "Compiling ~a~n" path)
-      (managed-compile-zo path)))
-  (in-directory (build-path (read-config 'seashell-collects))))
+(parallel-compile-files
+  (filter
+    (lambda (path)
+      (and (file-exists? path)
+           (equal? (filename-extension path) #"rkt")))
+    (sequence->list (in-directory (build-path (read-config 'seashell-collects)))))
+  #:handler (lambda (_ type work msg out err)
+    (match type
+      ['start (void)]
+      ['done (printf " Made ~a\n" work)]
+      ['output (printf " Output from: ~a\n~a~a" work out err)]
+      [else (printf " Error compiling ~a\n~a\n~a~a"
+                    work
+                    msg
+                    out
+                    err)
+            (exit 1)])))
