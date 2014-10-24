1bbc4e7946efde64ee4250508ae75d6673929993
Edward Lee <e45lee@uwaterloo.ca>
2014-10-24 15:31:48 -0400

    [backend] Add stdout to test output.

--- src/collects/seashell/backend/dispatch.rkt
+++ src/collects/seashell/backend/dispatch.rkt
@@ -97,7 +97,7 @@
                                          (result . #hash((pid . ,pid) (test_name . ,test-name) (result . "no-expect")
                                                          (stdout . ,(bytes->string/utf-8 stdout #\?))
                                                          (stderr . ,(bytes->string/utf-8 stderr #\?))))))]
-         [(list pid test-name "failed" diff stderr)
+         [(list pid test-name "failed" diff stderr stdout)
           (send-message `#hash((id . -4) (success . #t)
                                          (result . #hash((pid . ,pid) (test_name . ,test-name) (result . "failed")
                                                          (diff . ,(map
@@ -109,6 +109,7 @@
                                                                      x)
                                                                 (bytes->string/utf-8 x #\?)))
                                                             diff))
+                                                         (stdout . ,(bytes->string/utf-8 stdout #\?))
                                                          (stderr . ,(bytes->string/utf-8 stderr #\?))))))])))
   
   ;; (project-output-runner-thread)
