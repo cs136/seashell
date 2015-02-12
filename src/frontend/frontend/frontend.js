a26c1c9a21023a4c666e6fa4cb90ffdea201ef10
Edward Lee <e45lee@uwaterloo.ca>
2015-02-12 11:57:57 -0500

    [frontend] Wide-mode Seashell.

--- src/frontend/frontend/frontend.js
+++ src/frontend/frontend/frontend.js
@@ -745,7 +745,7 @@ angular.module('frontend-app', ['seashell-websocket', 'seashell-projects', 'jque
         });
         // Resize events
         function onResize() {
-          var min_height = 500, margin_bottom = 60;
+          var min_height = 500, margin_bottom = 30;
           var min_y_element = $('#editor > .CodeMirror');
           var h = Math.max($($window).height() - (min_y_element.offset().top - $($window).scrollTop()) - margin_bottom,
                            min_height);
@@ -754,6 +754,8 @@ angular.module('frontend-app', ['seashell-websocket', 'seashell-projects', 'jque
             .height(Math.floor(narrow ? h * 0.7 : h) - $('#current-file-controls').outerHeight()); 
           $('#console > .CodeMirror')
             .height((narrow ? (h * 0.3 - $('#console-title').outerHeight()) : 1 + h) - $('.console-input').outerHeight());
+          if(editor)
+            editor.refresh();
         }
         $scope.$on('window-resized', onResize);
       
