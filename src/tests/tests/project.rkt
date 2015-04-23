1ad7feabf153100aeb6baa9b98b6d4cb1344c48f
Kaleb Alway <kpalway@uwaterloo.ca>
2015-04-23 11:24:23 -0400

    Two more project.rkt tests

--- src/tests/tests/project.rkt
+++ src/tests/tests/project.rkt
@@ -19,8 +19,19 @@
       (define-values (_ hsh) (compile-and-run-project "foo" "test.c" '()))
       (sync (program-wait-evt (hash-ref hsh 'pid))))
 
+    (test-case "Get a Compilation Error"
+      (with-output-to-file (check-and-build-path (build-project-path "foo") "error.c")
+        (thunk (display "great code;")))
+      (define-values (res hsh) (compile-and-run-project "foo" "error.c" '()))
+      (check-false res)
+      (check string=? (hash-ref hsh 'status) "compile-failed"))
+
     (test-case "Export a Project"
       (export-project "foo"))
+
+    (test-case "Most Recently Used"
+      (update-most-recently-used "foo" #f '("fexists" "error.c") "some data")
+      (check string=? (get-most-recently-used "foo" #f) "some data"))
 
     (test-case "Delete a Project"
       (delete-project "foo")
