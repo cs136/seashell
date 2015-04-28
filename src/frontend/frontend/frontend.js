8c2e845e5cc6b2602497756eed86ca8b0dc747b0
Kaleb Alway <kpalway@uwaterloo.ca>
2015-04-28 11:31:35 -0400

    Stack buffer overflow error parsing

--- src/frontend/frontend/frontend.js
+++ src/frontend/frontend/frontend.js
@@ -543,6 +543,11 @@ angular.module('frontend-app', ['seashell-websocket', 'seashell-projects', 'jque
               /\/([^\/]+:[0-9]+)$/.exec(asan_contents[2])[1],
               /0x[0-9a-f]{12}/.exec(asan_contents[1])));
           }
+          else if(/stack-buffer-(over|under)flow /.test(asan_contents[1])) { // stack buffer overflow
+            self._write(sprintf("%s: Stack buffer overflow on address %s. Check array indices.\n",
+              /\/([^\/]+:[0-9]+)$/.exec(asan_contents[3])[1],
+              /0x[0-9a-f]{12}/.exec(asan_contents[1])));
+          }
           else { // else print usual message
             _.each(asan_contents, function(line) {
               self._write(line + "\n");
