e720fe6c502bcfc71f58b31a2fbfb017a00b1ef6
Kaleb Alway <kpalway@uwaterloo.ca>
2015-05-11 10:32:27 -0400

    Separate filters from frontend.js

--- src/frontend/frontend/frontend.js
+++ src/frontend/frontend/frontend.js
@@ -15,43 +15,12 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with self program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* jshint supernew: true */
 angular.module('frontend-app', ['seashell-websocket', 'seashell-projects', 'jquery-cookie', 'ui.router',
-    'ui.bootstrap', 'ui.codemirror', 'cfp.hotkeys'])
-  .filter('projectFilter', function() {
-    return function(input, type){
-      var pattAssn = new RegExp('^A[0-9]+$');
-      var pattTut = new RegExp('^Tut[0-9]+$');
-      var pattLec = new RegExp('^Lec[0-9]+$');
-      var out = [];
-      for(var i = 0; i < input.length; i++){
-        if(type === 'A'){
-          if(pattAssn.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
-        else if(type === 'TUT'){
-          if(pattTut.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
-        else if(type === 'LEC'){
-          if(pattLec.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
-        else {
-          if(!pattAssn.test(input[i][0]) && !pattTut.test(input[i][0]) && !pattLec.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
-      }
-      return out;
-    };
-  })
+    'ui.bootstrap', 'ui.codemirror', 'cfp.hotkeys', 'seashell-filters'])
   // Error service.
   .service('error-service', ['$rootScope', '$timeout', '$sce',
     function ($rootScope, $timeout, $sce) {
