fb53b37e40970327a606e113465e9bdb54db8222
Kaleb Alway <kpalway@uwaterloo.ca>
2015-05-13 14:08:45 -0400

    Separate console service

--- src/frontend/frontend/settings.js
+++ src/frontend/frontend/settings.js
@@ -1,5 +1,5 @@
 /**
- * Seashell's frontend.
+ * Seashell's frontend settings service
  * Copyright (C) 2013-2015 The Seashell Maintainers.
  *
  * This program is free software: you can redistribute it and/or modify
