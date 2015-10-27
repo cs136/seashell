9b266188d14eb54acfcbc183a18b524f90c1a168
Charlie Wang <charlie.wang@uwaterloo.ca>
2015-10-27 16:54:08 -0400

    fix ctrl+d shortcut, batch auto-indent

--- src/frontend/frontend/file.js
+++ src/frontend/frontend/file.js
@@ -269,6 +269,7 @@ angular.module('frontend-app')
           var main_hotkeys = [{
             combo: 'ctrl+d',
             description: 'Sends EOF',
+            allowIn: ['INPUT', 'TEXTAREA'],
             callback: function(evt) {
               evt.preventDefault();
               self.sendEOF();
@@ -438,9 +439,10 @@ angular.module('frontend-app')
         };
 
         self.indentAll = function() {
-          var lineCount = self.editor.lineCount();
-          for (var i = 0; i < lineCount; i++)
-            self.editor.indentLine(i);
+          self.editor.operation(function () {
+            var lineCount = self.editor.lineCount();
+            for (var i = 0; i < lineCount; i++) { self.editor.indentLine(i); }
+          });
         };
 
         self.userInput = "";
