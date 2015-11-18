0ae5604c1224ff30a1453c86eda15cadac4f8a02
Charlie Wang <charlie.wang@uwaterloo.ca>
2015-11-18 09:56:29 -0500

    add auto-focus to modal forms

--- src/frontend/frontend/directives.js
+++ src/frontend/frontend/directives.js
@@ -52,4 +52,17 @@ angular.module('frontend-app')
             $timeout(function () {elem[0].focus();});
         });
      };
-  }]);
+  }])
+  .directive('focusMe', function($timeout, $parse) {
+     return {
+       link: function(scope, element, attrs) {
+         var model = $parse(attrs.focusMe);
+         scope.$watch(model, function(value) {
+           if(value === true) { 
+             $timeout(function() {
+               element[0].focus(); 
+             });
+           }
+         });
+       }; 
+ }});
