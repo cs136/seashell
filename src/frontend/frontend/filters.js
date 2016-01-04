9ff23a948d05fae6bd71eef5a92ffed1a37d46cb
Charlie Wang <charlie.wang@uwaterloo.ca>
2016-01-04 14:21:50 -0500

    [frontend] refactor code for sorting projects

--- src/frontend/frontend/filters.js
+++ src/frontend/frontend/filters.js
@@ -21,36 +21,48 @@
 
 angular.module('frontend-app')
   .filter('projectFilter', function() {
-    return function(input, type){
+    return function(input, type) {
       var pattAssn = new RegExp('^A[0-9]+$');
       var pattTut = new RegExp('^Tut([0-9]+|[0-9]+Sol)$');
       var pattLec = new RegExp('^Lec[0-9]+$');
+
       var out = [];
-      for(var i = 0; i < input.length; i++){
-        if(type === 'A'){
-          if(pattAssn.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
-        else if(type === 'TUT'){
-          if(pattTut.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
-        else if(type === 'LEC'){
-          if(pattLec.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
-        else {
-          if(!pattAssn.test(input[i][0]) && !pattTut.test(input[i][0]) && !pattLec.test(input[i][0])){
-            out.push(input[i][0]);
-          }
-        }
+      var pattern;
+      var isNone = false;
+      switch (type) {
+        case 'A':
+          pattern = pattAssn;
+          break;
+        case 'LEC':
+          pattern = pattLec;
+          break;
+        case 'TUT':
+          pattern = pattTut;
+          break;
+        case 'NONE':
+          isNone = true;
       }
+
+      out = _.filter(_.map(input, function(x) {
+          return x[0]
+        }),
+        function(name) {
+          var assnMatch = pattAssn.test(name);
+          var lecMatch = pattLec.test(name);
+          var tutMatch = pattTut.test(name);
+
+          if (isNone && !assnMatch && !lecMatch && !tutMatch) {
+            return true;
+          } else if (!isNone && pattern.test(name)) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+
       return out;
     };
   })
-  .filter('escapeFilter', function () {
+  .filter('escapeFilter', function() {
     return window.escape;
   });
