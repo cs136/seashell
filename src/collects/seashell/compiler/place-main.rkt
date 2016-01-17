348204381c604367bbfdc5d9720173f775d8f616
Edward Lee <e45lee@uwaterloo.ca>
2016-01-17 16:23:15 -0500

    [backend] Compiler in Typed Racket.  Some issues with linkage.

--- src/collects/seashell/compiler/place-main.rkt
+++ src/collects/seashell/compiler/place-main.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang typed/racket
 ;; Seashell's Clang interface.
 ;; Copyright (C) 2013-2015 The Seashell Maintainers.
 ;;
@@ -18,33 +18,42 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (require seashell/compiler/compiler
          seashell/log
-         seashell/seashell-config
-         racket/place)
+         (submod seashell/seashell-config typed))
 (provide seashell-compiler-place)
 
 ;; (seashell-compiler-place/thread write-end . args)
 ;; Thread that actually processes the compilation request.
-(define (seashell-compiler-place/thread write-end . args)
+(: seashell-compiler-place/thread (-> Place-Channel (Listof String) (Listof String) (Listof Path) (Listof Path) Thread))
+(define (seashell-compiler-place/thread write-end cflags ldflags cfiles ofiles)
   (thread
     (lambda ()
       (with-handlers
         ([exn:fail?
-          (lambda (exn) (place-channel-put write-end (list #t #f (exn-message exn))))])
-        (define-values (result data) (apply seashell-compile-files args))
-        (place-channel-put write-end (list #f result data))))))
-       
+          (lambda ([exn : exn]) (place-channel-put write-end (list #t #f (exn-message exn)))
+            (logf 'error "Exception raised in compilation place ~a: ~a" (exn-message exn) (format-stack-trace (exn-continuation-marks exn))))])
+        (define-values (result data) (seashell-compile-files cflags ldflags cfiles ofiles))
+        (place-channel-put write-end (list #f result (seashell-diagnostic-table->bytes data)))))))
+
 ;; (seashell-compiler-place channel)
 ;; Invokes seashell-compile-files in a separate place,
 ;; preserving parallelism in Racket.
 ;;
 ;; This is the main function for the place.
+(: seashell-compiler-place (-> Place-Channel Any))
 (define (seashell-compiler-place channel)
   ;; These two things should not fail.
   (config-refresh!)
   (standard-logger-setup)
-  (let loop ()
+  (let loop : Any ()
     (with-handlers
       ([exn:fail?
-        (lambda (exn) (place-channel-put channel (list #t #f (exn-message exn))))])
-      (apply seashell-compiler-place/thread (place-channel-get channel)))
+        (lambda ([exn : exn]) (place-channel-put channel (list #t #f (exn-message exn)))
+          (logf 'error "Exception raised in compilation place ~a: ~a" (exn-message exn) (format-stack-trace (exn-continuation-marks exn))))])
+      (match-define (list chan cflags ldflags cfiles ofiles)
+        (place-channel-get channel))
+      (seashell-compiler-place/thread (cast chan Place-Channel)
+                                      (cast cflags (Listof String))
+                                      (cast ldflags (Listof String))
+                                      (cast cfiles (Listof Path))
+                                      (cast ofiles (Listof Path))))
     (loop)))
--- src/collects/seashell/compiler/place.rkt
+++ src/collects/seashell/compiler/place.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang typed/racket
 ;; Seashell's Clang interface.
 ;; Copyright (C) 2013-2015 The Seashell Maintainers.
 ;;
@@ -16,35 +16,35 @@
 ;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-(require seashell/compiler/compiler
-         racket/contract
-         racket/match
-         racket/place
-         racket/bool
-         seashell/compiler/ffi)
+(require seashell/compiler/compiler)
 (provide seashell-compile-files/place
          seashell-compile-place/init)
 
+(: compiler-place (U False Place))
 (define compiler-place #f)
+
 ;; seashell-compile-files/place 
 ;; Invokes seashell-compile-files in a separate place,
 ;; preserving parallelism in Racket.
-(define/contract (seashell-compile-files/place user-cflags user-ldflags sources objects)
-  (-> (listof string?) (listof string?) (listof path?) (listof path?)
-      (values (or/c bytes? false?) (hash/c path? (listof seashell-diagnostic?))))
-  (cond
-    [(and compiler-place (not (sync/timeout 0 (place-dead-evt compiler-place))))
-      (define-values (read-end write-end) (place-channel))
-      (place-channel-put compiler-place 
-                         (list write-end user-cflags user-ldflags sources objects))
-      (match-define 
-        (list exn? result data)
-        (place-channel-get read-end))
-      (when exn?
-            (raise (exn:fail (format "Exception raised in compiler place: ~a!" data)
-                             (current-continuation-marks))))
-      (values result data)]
-    [else (values #f (make-hash))]))
+(: seashell-compile-files/place (-> (Listof String) (Listof String) (Listof Path) (Listof Path)
+                                    (Values (U Bytes False) Seashell-Diagnostic-Table)))
+(define (seashell-compile-files/place user-cflags user-ldflags sources objects)
+  (let ([compiler-place compiler-place])
+    (assert (place? compiler-place))
+    (cond
+      [(and compiler-place (not (sync/timeout 0 (place-dead-evt compiler-place))))
+       (define-values (read-end write-end) (place-channel))
+       (place-channel-put compiler-place
+                          (list write-end user-cflags user-ldflags sources objects))
+       (match-define
+         (list exn? result data)
+         (place-channel-get read-end))
+       (when exn?
+         (raise (exn:fail (format "Exception raised in compiler place: ~a!" data)
+                          (current-continuation-marks))))
+       (values (cast result (U False Bytes))
+               (bytes->seashell-diagnostic-table (cast data Bytes)))]
+      [else (values #f (make-hash))])))
 
 ;; seashell-compile-place/init
 ;; Sets up the place for compilation.
@@ -57,4 +57,5 @@
                     #:out (current-error-port)
                     #:err (current-error-port)))
   (set! compiler-place place)
+  (assert (output-port? in))
   (close-output-port in))
