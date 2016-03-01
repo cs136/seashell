32bd0fba013851e9e1d5716b98ae7f05be78f1ce
Edward Lee <e45lee@uwaterloo.ca>
2016-03-01 12:51:21 -0500

    [backend] Add missing utils file.

---
+++ src/collects/seashell/utils.rkt
@@ -0,0 +1,29 @@
+#lang typed/racket
+;; Seashell's compiler system.
+;; Copyright (C) 2013-2015 The Seashell Maintainers.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(provide check-eof)
+
+;; Handy syntax rule for EOF checking
+(: check-eof (All (X) (-> (U EOF X) X)))
+(define (check-eof x)
+  (cond
+    [(eof-object? x)
+      (raise (exn:fail (format "read: Unexpected end of file!")
+                               (current-continuation-marks)))]
+    [else
+      x]))
