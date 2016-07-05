a4fcdc33d9d29e95d96ea8cb03bf13f48e8ef3c7
Edward Lee <e45lee@uwaterloo.ca>
2016-07-05 13:22:50 -0400

    [frontend] Fix typo.

--- src/frontend/frontend/file.js
+++ src/frontend/frontend/file.js
@@ -374,7 +374,7 @@ angular.module('frontend-app')
         self.renameFile = function() {
           renameModal(self.project, self.question, self.folder, self.file, function(newName) {
             var path = newName.split("/");
-            $scope.$parent.refresh();
+            $scope.$parent.editView.refresh();
             $state.go("edit-project.editor.file", {
               question:(path[0]=="common"?self.question:path[0]),
               part:(path.length>2?path[1]:(path[0]=="common"?"common":"question")),
@@ -387,7 +387,7 @@ angular.module('frontend-app')
             .then(function() {
               self.project.deleteFile(self.question, self.folder, self.file)
                 .then(function() {
-                  $scope.$parent.refresh();
+                  $scope.$parent.editView.refresh();
                   $state.go("edit-project.editor");
                   self.refreshRunner();
                 });
