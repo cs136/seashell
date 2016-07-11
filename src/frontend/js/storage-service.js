13907f40cd0f6d176f5e5e743372c591a28d82c5
Edward Lee <e45lee@uwaterloo.ca>
2016-07-10 22:25:55 -0400

    [frontend] ES6=>ES5 storage service -- phantomjs does not support yet.

--- src/frontend/js/storage-service.js
+++ src/frontend/js/storage-service.js
@@ -1,5 +1,23 @@
-  /* jslint esversion: 6 */
-  angular.module('seashell-local-files', [])
+/*
+ * Angular bindings for the Seashell WebSocket client.
+ * Copyright (C) 2013-2015 The Seashell Maintainers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with self program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ angular.module('seashell-local-files', [])
     .service('localfiles', ['$q', '$cookies',
       function($q, $cookies) {
         "use strict";
@@ -19,15 +37,15 @@
          * @param {string} file_history: The history of the file.
          * @param {string || any false value} checksum : The online checksum of the file, false to not update (offline write).
          */
-        self.writeFile = (name, file, contents, history, checksum) => {
-          let offline_checksum = md5(contents);
-          let key = [name, file];
-          return self.database.transaction('rw', self.database.changelog, self.database.files, () => {
+        self.writeFile = function (name, file, contents, history, checksum) {
+          var offline_checksum = md5(contents);
+          var key = [name, file];
+          return self.database.transaction('rw', self.database.changelog, self.database.files, function () {
             if (checksum !== undefined) {
               return self.database.files.update(key,
                   {contents: contents, history: history, checksum: checksum,
                    last_modified: new Date()})
-                .then((result) => {
+                .then(function (result) {
                   if (!result) {
                     throw sprintf("Storage.writeFile: file %s/%s not found!", name, file);
                   }
@@ -35,7 +53,7 @@
                 });
             } else {
               return self.database.files.get(key)
-                .then((current) => {
+                .then(function (current) {
                   self.database.changelog.add({file:{project: name, file: file, checksum: current.checksum},
                                                type: "editFile",
                                                history: history,
@@ -49,22 +67,22 @@
           });
         };
 
-        self.readFile = (name, file) => {
-          let key = [name, file];
+        self.readFile = function (name, file) {
+          var key = [name, file];
           return self.database.files.get(key)
-            .then((result) => {
+            .then(function (result) {
               return {data: result.contents, history: result.history};
             });
         };
 
-        self.deleteFile = (name, file, online) => {
-          let key = [name, file];
-          return self.database.transaction('rw', self.database.changelog, self.database.files, () => {
+        self.deleteFile = function (name, file, online) {
+          var key = [name, file];
+          return self.database.transaction('rw', self.database.changelog, self.database.files, function () {
             if (online !== undefined) {
               return self.database.files.delete(key);
             } else {
               return self.database.files.get(key)
-                .then((current) => {
+                .then(function (current) {
                   self.database.changelog.add({file:{project: name, file: file, checksum: current.checksum},
                                                type: "deleteFile"});
                   return self.database.files.delete(key);
@@ -73,11 +91,11 @@
           });
         };
 
-        self.renameFile = (name, file, new_file, checksum) => {
-          let key = [name, file];
-          return self.database.transaction('rw', self.database.files, self.database.changelog, () => {
+        self.renameFile = function (name, file, new_file, checksum) {
+          var key = [name, file];
+          return self.database.transaction('rw', self.database.files, self.database.changelog, function () {
             return self.database.files.get({project: name, file: file})
-              .then((result) => {
+              .then(function (result) {
                 self.deleteFile(name, file, checksum);
                 result.file = new_file;
                 if (checksum !== undefined) {
@@ -93,64 +111,64 @@
           });
         };
 
-        self.getFileToRun = (name, question) => {
+        self.getFileToRun = function (name, question) {
           return self.database.projects.get(name)
-            .then((project) => {
+            .then(function (project) {
               return project.settings.runner_files[question];
             });
         };
 
-        self.setFileToRun = (name, question, folder, file) => {
+        self.setFileToRun = function (name, question, folder, file) {
           if (folder === "tests") {
             throw sprintf("Storage.setFileToRun: folder cannot be tests/!");
           }
-          return self.database.transaction('rw', self.database.projects, () => {
-            self.database.projects.get(name).then((current) => {
+          return self.database.transaction('rw', self.database.projects, function () {
+            self.database.projects.get(name).then(function (current) {
               current.settings.runner_files[question] = folder === "question" ? sprintf("%s/%s", question, file) : sprintf("%s/%s", folder, file);
               return self.database.projects.put(current);
             });
           });
         };
 
-        self.listProject = (name) => {
+        self.listProject = function (name) {
           return self.database.files.where('project').equals(name).toArray(
-          (files) => {
+          function (files) {
             return files.map(
-              (file) => {
+              function (file) {
                 return [file.file, false, file.last_modified.getUTCMilliseconds(), file.checksum];
               });
           });
       };
 
-      self.listAllProjectsForSync = () => {
+      self.listAllProjectsForSync = function () {
         return self.database.files.toArray(
-        (files) => {
+        function (files) {
           return files.map(
-            (file) => {
+            function (file) {
               return {project: file.project, file:file.file, checksum: file.checksum};
             });
         });
       };
 
-      self.newDirectory = (name, path) => {
-        return new Dexie.Promise((resolve, reject) => {resolve(true);});
+      self.newDirectory = function (name, path) {
+        return new Dexie.Promise(function (resolve, reject) {resolve(true);});
       };
 
-      self.deleteDirectory = (name, path) => {
-        return new Dexie.Promise((resolve, reject) => {resolve(true);});
+      self.deleteDirectory = function (name, path) {
+        return new Dexie.Promise(function (resolve, reject) {resolve(true);});
       };
 
-      self.newFile = (name, file, contents, encoding, normalize, online_checksum) => {
-        let checksum = (typeof contents === "string" && md5(contents)) || online_checksum || "";
-        let key = [name, file];
-        return self.database.transaction('rw', self.database.changelog, self.database.files, () => {
+      self.newFile = function (name, file, contents, encoding, normalize, online_checksum) {
+        var checksum = (typeof contents === "string" && md5(contents)) || online_checksum || "";
+        var key = [name, file];
+        return self.database.transaction('rw', self.database.changelog, self.database.files, function () {
           if (online_checksum !== undefined) {
             // TODO: Set history when syncing.
             return self.database.files.add({project: name, file: file,
                                             contents: contents, history: "",
                                             checksum: checksum,
                                             last_modified: new Date()})
-              .then(() => {
+              .then(function () {
                 return checksum;
               });
           } else {
@@ -165,35 +183,35 @@
         });
       };
 
-      self.newProject = (name) => {
+      self.newProject = function (name) {
         return self.database.projects.add({name: name, settings: {runner_files: {}}, last_modified: new Date()});
       };
 
-      self.deleteProject = (name, online) => {
-        return self.database.transaction('rw', self.database.files, self.database.changelog, self.database.projects, () => {
-          self.database.files.where('project').equals(name).each((file) => {
+      self.deleteProject = function (name, online) {
+        return self.database.transaction('rw', self.database.files, self.database.changelog, self.database.projects, function () {
+          self.database.files.where('project').equals(name).each(function (file) {
             self.deleteFile(name, file.file, online);
           });
           self.database.projects.delete(name);
         });
       };
 
-      self.getProjects = () => {
-        return self.database.projects.toCollection().toArray((projects) => {
-          return projects.map((project) => {
+      self.getProjects = function () {
+        return self.database.projects.toCollection().toArray(function (projects) {
+          return projects.map(function (project) {
             return [project.name, project.last_modified.getUTCMilliseconds()];
           });
         });
       };
 
-      self.applyChanges = (changes, newProjects, deletedProjects) => {
-        return self.database.transaction('rw', self.database.files, self.database.changelog, self.database.projects, () => {
+      self.applyChanges = function (changes, newProjects, deletedProjects) {
+        return self.database.transaction('rw', self.database.files, self.database.changelog, self.database.projects, function () {
           // TODO: how to sync project settings?  Probably with last-modified-time...
           // TODO: send back project settings with project
-          newProjects.forEach((project) => {
+          newProjects.forEach(function (project) {
             self.newProject(project);
           });
-          changes.forEach((change) => {
+          changes.forEach(function (change) {
             if (change.type === "deleteFile") {
               self.deleteFile(change.file.project, change.file.file, true);
             } else if (change.type === "editFile") {
@@ -204,14 +222,14 @@
               throw sprintf("applyChanges: unknown change %s!", change);
             }
           });
-          deletedProjects.forEach((project) => {
+          deletedProjects.forEach(function (project) {
             self.deleteProject(name, true);
           });
           self.database.changelog.clear();
         });
       };
 
-      self.getOfflineChanges = () => {
+      self.getOfflineChanges = function () {
         return self.database.changelog.toArray();
       };
     }
