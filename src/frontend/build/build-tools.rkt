9826c16c72b0ed52241a5a7ee52710ad13cf9d03
Edward Lee <e45lee@uwaterloo.ca>
2016-08-19 15:30:02 -0400

    [frontend] Automatically generate HTML5 manifest, scripts, and styles.

---
+++ src/frontend/build/build-tools.rkt
@@ -0,0 +1,77 @@
+#lang typed/racket
+(require typed/racket/date)
+(provide prepare-compile-file make-script-section make-style-section)
+
+(: read-resource-file (-> Path-String (Listof String)))
+(define (read-resource-file path)
+  (with-input-from-file path
+    (thunk
+      (for/list : (Listof String)
+        ([line (in-lines)]
+         #:unless (regexp-match "^$|^#" line))
+        line))))
+
+(: local-resource? (-> String Boolean))
+(define (local-resource? resource)
+  (not (regexp-match "^//" resource)))
+
+(: compile-js-resources (->* ((Listof String)) (Boolean String) (Listof String)))
+(define (compile-js-resources resources [minify? #f] [target ""])
+  (define local-resources (filter local-resource? resources))
+  (define other-resources (filter (compose not local-resource?) resources))
+  (cond
+    [(not minify?) resources]
+    [else
+      (define target-js (string-append target ".min.js"))
+      (define target-js-map (string-append target ".js.map"))
+      (define uglifyjs (find-executable-path "uglifyjs"))
+      (assert (path? uglifyjs))
+      (apply system* (list* (path->string uglifyjs)
+                            "-o" target-js
+                            "--source-map" target-js-map
+                            "--compress"
+                            "--mangle"
+                            "--keep-fnames"
+                            "--"
+                            local-resources))
+      (list* target-js other-resources)]))
+
+(: make-manifest-file (-> Path-String (Listof String) Any))
+(define (make-manifest-file target resources)
+  (with-output-to-file target #:exists 'truncate
+    (thunk
+      (printf "CACHE MANIFEST~n")
+      (printf "# Generated on: ~a~n" (date->string (current-date) #t))
+      (for ([r resources])
+        (printf "~a~n" r)))))
+
+(: prepare-compile-file (->* (String Path-String Path-String Path-String)
+                             (Boolean)
+                             (Values (Listof String) (Listof String))))
+(define (prepare-compile-file target js-resource-file css-resource-file extra-resource-file [minify? #f])
+  (define js-resources (read-resource-file js-resource-file))
+  (define css-resources (read-resource-file css-resource-file))
+  (define extra-resources (read-resource-file extra-resource-file))
+  (define manifest-target (string-append target ".manifest"))
+  (cond
+    [(not minify?)
+      (make-manifest-file manifest-target (append js-resources (append css-resources (append extra-resources))))
+     (values js-resources css-resources)]
+    [else
+      (define minified-js-resources (compile-js-resources js-resources #t target))
+      (make-manifest-file manifest-target (append minified-js-resources (append css-resources (append extra-resources))))
+      (values minified-js-resources js-resources)]))
+
+(: make-script-section (-> (Listof String) String))
+(define (make-script-section jsfiles)
+  (with-output-to-string
+    (thunk
+      (for ([jsfile jsfiles])
+        (printf "<script src=\"~a\"></script>~n" jsfile)))))
+
+(: make-style-section (-> (Listof String) String))
+(define (make-style-section cssfiles)
+  (with-output-to-string
+    (thunk
+      (for ([cssfile cssfiles])
+        (printf "<link rel=\"stylesheet\" href=\"~a\"></script>~n" cssfile)))))
