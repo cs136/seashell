2825a8eadffcfff3e7aa25e920aaf1eff32acc5b
Kaleb Alway <kpalway@uwaterloo.ca>
2016-10-07 01:13:55 -0400

    Get seashell-cli to reliably create .ll files with a terrible hack

--- src/backend/compiler/compiler.cc
+++ src/backend/compiler/compiler.cc
@@ -27,6 +27,9 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <mutex>
+#include <chrono>
+#include <thread>
 
 #include <libgen.h>
 #include <unistd.h>
@@ -38,6 +41,11 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+
+#define STD_OUT_FD STDOUT_FILENO
+#define STD_IN_FD STDIN_FILENO
+#define STD_ERR_FD STDERR_FILENO
 
 #ifndef __EMSCRIPTEN__
 #include <seashell-config.h>
@@ -557,18 +565,176 @@ std::string seashell_compiler_get_object(struct seashell_compiler* compiler) {
 #endif
 }
 
-class SeashellPrintModulePass : public llvm::ModulePass {
-  llvm::raw_ostream &_os;
-
+// This class deals with piping stderr to a string.
+// Taken from a Stack Overflow post and modified
+// TODO: Change to suit more specifically our purpose here
+//  or even better, find a way to get the IR from Clang other than
+//  the dump() method to stderr...
+class StdCapture
+{
 public:
-  SeashellPrintModulePass(char &pid, llvm::raw_ostream &os) : ModulePass(pid), _os(os) { };
+    static void Init()
+    {
+        // make stdout & stderr streams unbuffered
+        // so that we don't need to flush the streams
+        // before capture and after capture 
+        // (fflush can cause a deadlock if the stream is currently being 
+        std::lock_guard<std::mutex> lock(m_mutex);
+        setvbuf(stdout,NULL,_IONBF,0);
+        setvbuf(stderr,NULL,_IONBF,0);
+    }
 
-  bool runOnModule(llvm::Module &M) {
-    llvm::ModulePass *printPass = (ModulePass*)createPrinterPass(_os, "banner");
-    printPass->runOnModule(M);
-    return false;
-  };
+    static void BeginCapture()
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        if (m_capturing)
+            return;
+
+        secure_pipe(m_pipe);
+        m_oldStdOut = secure_dup(STD_OUT_FD);
+        m_oldStdErr = secure_dup(STD_ERR_FD);
+        secure_dup2(m_pipe[WRITE],STD_OUT_FD);
+        secure_dup2(m_pipe[WRITE],STD_ERR_FD);
+        m_capturing = true;
+#ifndef _MSC_VER
+        secure_close(m_pipe[WRITE]);
+#endif
+    }
+    static bool IsCapturing()
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_capturing;
+    }
+    static bool EndCapture()
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        if (!m_capturing)
+            return false;
+
+        m_captured.clear();
+        secure_dup2(m_oldStdOut, STD_OUT_FD);
+        secure_dup2(m_oldStdErr, STD_ERR_FD);
+
+        const int bufSize = 1025;
+        char buf[bufSize];
+        int bytesRead = 0;
+        bool fd_blocked(false);
+        do
+        {
+            bytesRead = 0;
+            fd_blocked = false;
+#ifdef _MSC_VER
+            if (!eof(m_pipe[READ]))
+                bytesRead = read(m_pipe[READ], buf, bufSize-1);
+#else
+            bytesRead = read(m_pipe[READ], buf, bufSize-1);
+#endif
+            if (bytesRead > 0)
+            {
+                buf[bytesRead] = 0;
+                m_captured += buf;
+            }
+            else if (bytesRead < 0)
+            {
+                fd_blocked = (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR);
+                if (fd_blocked)
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+        }
+        while(fd_blocked || bytesRead == (bufSize-1));
+
+        secure_close(m_oldStdOut);
+        secure_close(m_oldStdErr);
+        secure_close(m_pipe[READ]);
+#ifdef _MSC_VER
+        secure_close(m_pipe[WRITE]);
+#endif
+        m_capturing = false;
+    }
+    static std::string GetCapture()
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_captured;
+    }
+private:
+    enum PIPES { READ, WRITE };
+
+    static int secure_dup(int src)
+    {
+        int ret = -1;
+        bool fd_blocked = false;
+        do
+        {
+             ret = dup(src);
+             fd_blocked = (errno == EINTR ||  errno == EBUSY);
+             if (fd_blocked)
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        while (ret < 0);
+        return ret;
+    }
+    static void secure_pipe(int * pipes)
+    {
+        int ret = -1;
+        bool fd_blocked = false;
+        do
+        {
+#ifdef _MSC_VER
+            ret = pipe(pipes, 65536, O_BINARY);
+#else
+            ret = pipe(pipes) == -1;
+#endif
+            fd_blocked = (errno == EINTR ||  errno == EBUSY);
+            if (fd_blocked)
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        while (ret < 0);
+    }
+    static void secure_dup2(int src, int dest)
+    {
+        int ret = -1;
+        bool fd_blocked = false;
+        do
+        {
+             ret = dup2(src,dest);
+             fd_blocked = (errno == EINTR ||  errno == EBUSY);
+             if (fd_blocked)
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        while (ret < 0);
+    }
+
+    static void secure_close(int & fd)
+    {
+        int ret = -1;
+        bool fd_blocked = false;
+        do
+        {
+             ret = close(fd);
+             fd_blocked = (errno == EINTR);
+             if (fd_blocked)
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        while (ret < 0);
+
+        fd = -1;
+    }
+
+    static int m_pipe[2];
+    static int m_oldStdOut;
+    static int m_oldStdErr;
+    static bool m_capturing;
+    static std::mutex m_mutex;
+    static std::string m_captured;
 };
+
+// actually define vars.
+int StdCapture::m_pipe[2];
+int StdCapture::m_oldStdOut;
+int StdCapture::m_oldStdErr;
+bool StdCapture::m_capturing;
+std::mutex StdCapture::m_mutex;
+std::string StdCapture::m_captured;
 
 /**
  * seashell_compiler_get_bytecode(struct seashell_compiler* compiler)
@@ -581,12 +747,15 @@ std::string seashell_compiler_get_bytecode(struct seashell_compiler *compiler) {
 #endif
   if(compiler->module.size() > 0) {
     std::string bc;
-    llvm::raw_string_ostream oss(bc);
-    char pid;
-    SeashellPrintModulePass spmp(pid, oss);
-    spmp.runOnModule(compiler->module);
-    //oss << compiler->module;
-    //std::cout << compiler->module.size() << std::endl;
+    // For now, we have this brutal hack.. redirect stderr to a stringstream,
+    //  then dump the module.. the usual print() method doesn't seem to work
+    //  reliably so I resorted to this. TODO figure out how the API should
+    //  work here
+    StdCapture::Init();
+    StdCapture::BeginCapture();
+    compiler->module.dump();
+    StdCapture::EndCapture();
+    bc = StdCapture::GetCapture();
 #ifndef __EMSCRIPTEN__
     return bc.c_str();
 #else
@@ -789,7 +958,7 @@ static int final_link_step (struct seashell_compiler* compiler)
   TargetMachine &Target = *target.get();
 
   /** Set up the code generator. */
-  PassManager PM;
+  llvm::legacy::PassManager PM;
 
   TargetLibraryInfo *TLI = new TargetLibraryInfo(TheTriple);
   PM.add(TLI);
