baf3b95c3905ae7543d601a63883ae4e48fb24c9
Kevin Pei <kevin-pei@hotmail.com>
2017-01-07 23:42:14 -0500

    improved build scripts with options, remove setup

--- build_tools/cmake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
-if [ -d "$SCRIPT_PATH/../_build/" ]; then rm -r "$SCRIPT_PATH/../_build/"; fi
-mkdir "$SCRIPT_PATH/../_build/"
-[ -e "$SCRIPT_PATH/../build.log" ] && rm "$SCRIPT_PATH/../build.log"
-echo "Cmaking Seashell to \`/_build\`... logging to /build.log"
-cd "$SCRIPT_PATH/../_build" && cmake "$SCRIPT_PATH/../" -DCMAKE_INSTALL_PREFIX="$SCRIPT_PATH/../_install"  > ../build.log 2>&1
-
