6f4f1a5160e9dc0af87b676667060b189b3b6b3e
Yu Li <YLiLarry@gmail.com>
2017-02-09 10:20:50 -0500

    code refactor

--- src/frontend/frontend/file.js
+++ src/frontend/frontend/file.js
@@ -101,6 +101,7 @@ angular.module('frontend-app')
             fn();
           }
         }
+        
         $scope.$on('run-when-saved', function (evt, fn) {
           runWhenSaved(fn);
         });
@@ -108,41 +109,18 @@ angular.module('frontend-app')
         self.onResize = function() {
           settings.settings.force_narrow = !(settings.settings.force_narrow);
           settings.save();
+          self.reRenderCM();
         };
-        // self.activateResize = function(){
-        //   settings.settings.force_narrow = !(settings.settings.force_narrow);
-        //   settings.save();
-        // };
-        // // Resize on window size change
-        // function onResize() {
-        //   var narrow = (settings.settings.force_narrow || $window.innerWidth < 992);
-        //   var min_height = 750, margin_bottom = 30;
-        //   var editor_elem = $window.document.querySelector("#editor > .CodeMirror");
-        //   var console_elem = $window.document.querySelector("#console > .CodeMirror");
-        //   // Run only when DOM is ready.
-        //   if (editor_elem && console_elem) {
-        //     var target_height = Math.max($window.innerHeight - editor_elem.getBoundingClientRect().top - margin_bottom, min_height);
-        //     var file_control_height = $window.document.querySelector('#current-file-controls').offsetHeight;
-        //     var console_input_height = $window.document.querySelector('#console-input').offsetHeight;
-        //     if (editor_elem)
-        //       editor_elem.style.height = sprintf("%fpx",
-        //         (narrow ? target_height * 0.5 : target_height) - file_control_height);
-        //     if (console_elem)
-        //       console_elem.style.height = sprintf("%fpx",
-        //         (narrow ? (target_height * 0.5 - file_control_height) : target_height) - console_input_height);
-        //     if(self.editor)
-        //       self.editor.refresh();
-        //     if(self.consoleEditor)
-        //       self.consoleEditor.refresh();
-        //     // Force the font size at any rate (and font name)
-        //     _.each($window.document.querySelectorAll('.CodeMirror'),
-        //         function (elem) {
-        //           elem.style['font-family'] = sprintf("%s, monospace", settings.settings.font);
-        //           elem.style['font-size'] = sprintf("%dpt", parseInt(settings.settings.font_size));
-        //         });
-        //   }
-        // }
-        // $scope.$on('window-resized', onResize);
+
+        self.reRenderCM = function() {
+          $(".CodeMirror *").css({
+            fontFamily: sprintf("%s, monospace", settings.settings.font),
+            fontSize: sprintf("%dpt", parseInt(settings.settings.font_size))
+          });
+          self.editor.refresh();
+          self.consoleEditor.refresh();
+        };
+        
         // Scope helper function follow.
         self.consoleLoad = function(console_cm) {
           self.consoleEditor = console_cm;
@@ -150,8 +128,9 @@ angular.module('frontend-app')
             var scr = self.consoleEditor.getScrollInfo();
             self.consoleEditor.scrollTo(scr.left, scr.height);
           });
-          // $timeout(self.onResize, 0);
+          // $timeout(self.reRenderCM, 0);
         };
+
         self.editorLoad = function(editor) {
           self.editor = editor;
           if (self.ext === "c" || self.ext === "h") {
@@ -253,8 +232,9 @@ angular.module('frontend-app')
           self.editor.on("cursorActivity", updateColNums);
           self.editor.on("focus", updateColNums);
           self.editor.on("blur", updateColNums);
-          // $timeout(self.onResize, 0);
+          // $timeout(self.reRenderCM, 0);
         };
+
         function betterTab(){
           if(self.editor.somethingSelected()){
             self.editor.indentSelection("add");
@@ -262,43 +242,66 @@ angular.module('frontend-app')
             self.editor.replaceSelection(Array(self.editor.getOption("indentUnit") + 1).join(" "), "end", "+input");
           }
         }
+
         function negTab(){
           if(self.editor.somethingSelected()){
             self.editor.indentSelection("subtract");
           }
         }
+
         function toggleEditorFullscreen(evt) {
           evt.preventDefault();
           self.consoleEditor.setOption('fullScreen', false);
           self.editor.focus();
           self.editor.setOption('fullScreen', !self.editor.getOption('fullScreen'));
+          self.reRenderCM();
         }
+
         function toggleConsoleFullscreen(evt) {
           evt.preventDefault();
           self.editor.setOption('fullScreen', false);
           self.consoleEditor.focus();
           self.consoleEditor.setOption('fullScreen', !self.consoleEditor.getOption('fullScreen'));
+          self.reRenderCM();
         }
+
         function quitFullscreen(evt) {
           evt.preventDefault();
           self.editor.setOption('fullScreen', false);
           self.consoleEditor.setOption('fullScreen', false);
+          self.reRenderCM();
         }
+
         function increaseFontSize(evt) {
           evt.preventDefault();
-          settings.settings.font_size = Math.min(settings.settings.font_size + 2, 100);
+          settings.settings.font_size = Math.min(settings.settings.font_size + 1, 50);
           settings.save();
+          self.reRenderCM();
         }
+
         function decreaseFontSize(evt) {
           evt.preventDefault();
-          settings.settings.font_size = Math.max(settings.settings.font_size - 2, 2);
+          settings.settings.font_size = Math.max(settings.settings.font_size - 1, 1);
           settings.save();
+          self.reRenderCM();
         }
+
         self.refreshSettings = function () {
           console.log("refresh editor settings");
           // var theme = settings.settings.theme_style === "light" ? "3024-day" : "3024-night";
           var theme = settings.settings.theme_style === "light" ? "default" : "3024-night";
           self.editorReadOnly = !self.ready || self.isBinaryFile || self.fileReadOnly;
+
+          var extraKeys = {
+            "Ctrl-Space": "autocomplete",
+            "Ctrl-I": self.indentAll,
+            // capture save shortcuts and ignore in the editor
+            "Ctrl-S": function() { },
+            "Cmd-S": function() { },
+            "Tab": betterTab,
+            "Shift-Tab": negTab,
+          };
+
           self.editorOptions = {
             scrollbarStyle: "overlay",
             autofocus: true,
@@ -312,16 +315,9 @@ angular.module('frontend-app')
             onLoad: self.editorLoad,
             matchBrackets: true,
             rulers: [80],
-            extraKeys: {
-              "Ctrl-Space": "autocomplete",
-              "Ctrl-I": self.indentAll,
-              // capture save shortcuts and ignore in the editor
-              "Ctrl-S": function() { },
-              "Cmd-S": function() { },
-              "Tab": betterTab,
-              "Shift-Tab": negTab,
-            }
+            extraKeys: extraKeys
           };
+
           self.consoleOptions = {
             scrollbarStyle: "overlay",
             lineWrapping: true,
@@ -329,9 +325,9 @@ angular.module('frontend-app')
             mode: "text/plain",
             theme: theme,
             onLoad: self.consoleLoad,
-            extraKeys: {
-            },
+            extraKeys: extraKeys
           };
+
           var main_hotkeys = [{
             combo: 'ctrl+d',
             description: 'Sends EOF',
@@ -374,6 +370,7 @@ angular.module('frontend-app')
             allowIn: ['INPUT', 'SELECT', 'TEXTAREA'],
             callback: quitFullscreen
           }];
+
           var vim_disabled_hotkeys = [{
             combo: 'ctrl+r',
             description: "Runs the program",
@@ -435,8 +432,9 @@ angular.module('frontend-app')
             }
             self.consoleEditor.refresh();
           }
-          // self.onResize();
+          // self.reRenderCM();
         };
+
         self.renameFile = function() {
           renameModal(self.project, self.question, self.folder, self.file, function(newName) {
             var path = newName.split("/");
@@ -447,7 +445,8 @@ angular.module('frontend-app')
               file:escape(path.length>2?path[2]:path[1])});
           });
         };
-	self.copyFile = function() {
+
+	      self.copyFile = function() {
           copyModal(self.project, self.question, self.folder, self.file, function(newName) {
             var path = newName.split("/");
             $scope.$parent.editView.refresh();
@@ -457,6 +456,7 @@ angular.module('frontend-app')
               file:escape(path.length>2?path[2]:path[1])});
           });
         };
+
         self.deleteFile = function() {
           confirmModal("Delete File", "Are you sure you want to delete '"+self.file+"'?")
             .then(function() {
@@ -639,7 +639,7 @@ angular.module('frontend-app')
         };
 
         // Initialization code goes here.
-        var settings_key = settings.addWatcher(function () {self.refreshSettings();}, true);
+        var settings_key = settings.addWatcher(self.refreshSettings, true);
         $scope.$on("$destroy", function() {
           if (self.timeout && self.ready) {
             $timeout.cancel(self.timeout);
