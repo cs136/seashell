45621defd0a0f4328be306b3ba6783c1028c988d
Kaleb Alway <kpalway@uwaterloo.ca>
2017-03-28 17:23:16 -0400

    Add ASAN output

--- src/frontend/src/helpers/Compiler/Interface.ts
+++ src/frontend/src/helpers/Compiler/Interface.ts
@@ -64,33 +64,70 @@ abstract class AbstractCompiler {
     });
   }
 
+  private outputASAN(ASAN: ASANOutput): string {
+    if (ASAN.error_type === "unknown" && ASAN.raw_message === "")
+      return "";
+    let output = "";
+    output += "Memory error occurred! Type of error: " + ASAN.error_type + "\n";
+    if (ASAN.call_stacks.length === 0) {
+      // If no call stack was sent back, fall back to showing the raw ASAN output
+      output += "Raw error message:\n";
+      output += ASAN.raw_message;
+    }
+    for (let stack = 0; stack < ASAN.call_stacks.length; stack++) {
+      const framelist = ASAN.call_stacks[stack].framelist;
+      const fmisc = ASAN.call_stacks[stack].misc;
+      const indent = framelist.length <= 1 ? "\t" : "\t  ";
+      for (let frame = 0; frame < framelist.length; frame++) {
+        output += indent + "frame " + framelist[frame].frame + ":" +
+          " function " + framelist[frame].function +
+          " in line " + framelist[frame].line +
+          ("column" in framelist[frame] ? ", column " + framelist[frame].column : "") + "\n";
+      }
+      for (let key in fmisc) {
+        output += "\t" + key.replace(/_/g, " ") + ": " + fmisc[key] + "\n";
+      }
+    }
+    for (let key in ASAN.misc) {
+      output += key.replace(/_/g, " ") + ": " + ASAN.misc[key] + "\n";
+    }
+    return output;
+  }
+
   protected handleIO(result: IOMessage): void {
+    let output = "";
     if (result.type === "stdout") {
       this.stdout += result.message;
       const spl = this.stdout.split("\n");
       for (let i = 0; i < spl.length - 1; i++) {
-        this.output(spl[i] + "\n");
+        output += spl[i] + "\n";
       }
       this.stdout = spl[spl.length - 1];
     } else if (result.type === "stderr") {
       this.stderr += result.message;
       const spl = this.stderr.split("\n");
       for (let i = 0; i < spl.length - 1; i++) {
-        this.output(spl[i] + "\n");
+        output += spl[i] + "\n";
       }
       this.stderr = spl[spl.length - 1];
     } else if (result.type === "done") {
-      this.output(this.stdout);
-      this.output(this.stderr);
+      output += this.stdout;
+      output += this.stderr;
       this.stdout = "";
       this.stderr = "";
-      this.output("Program finished with exit code " + result.status + ".\n");
+      const ASAN = result.asan_output ? JSON.parse(result.asan_output) : false;
+      if (ASAN) {
+        output += this.outputASAN(ASAN);
+      }
+      output += "Program finished with exit code " + result.status + ".\n";
       this.programDone(result.pid);
     }
+    this.output(output);
   }
 
   protected handleTest(result: TestMessage): void {
     let output = "";
+    const ASAN = result.asan_output ? JSON.parse(result.asan_output) : false;
     if (result.result === "passed") {
       output += "-------------------------------\n";
       output += "Test \"" + result.test_name + "\" passed.\n";
@@ -117,6 +154,10 @@ abstract class AbstractCompiler {
       output += "---\n";
       output += "Produced errors (stderr):\n";
       output += result.stderr;
+    }
+    if (ASAN) {
+      output += "AddressSanitizer Output:\n";
+      output += this.outputASAN(ASAN);
     }
     this.programDone(result.pid);
     this.output(output);
@@ -153,6 +194,7 @@ interface IOMessage {
   pid: number;
   type: string;
   status?: number;
+  asan_output?: string;
 }
 
 type DiffLine = string | [boolean, string];
@@ -165,4 +207,25 @@ interface TestMessage {
   test_name: string;
   diff?: DiffLine[];
   asan_output?: string;
+}
+
+interface ASANOutput {
+  call_stacks: ASANCallStack[];
+  misc: {[type: string]: string};
+  raw_message: string;
+  error_type: string;
+}
+
+interface ASANCallStack {
+  framelist: ASANStackFrame[];
+  misc: {[type: string]: string};
+}
+
+interface ASANStackFrame {
+  file: string;
+  function: string;
+  line: number;
+  column: number;
+  offset: string;
+  frame: number;
 }
