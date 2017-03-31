096681f6d76af3afc77f35f97148933eb21cecc4
Ryan Leung <rvleung@uwaterloo.ca>
2017-03-30 22:00:34 -0400

    hook up all the actions, except add/remove question and copy file. TODO save/restore open tabs, re-hook up to UI

--- src/frontend/src/actions/index.ts
+++ src/frontend/src/actions/index.ts
@@ -4,8 +4,7 @@ import { globalState } from "../reducers/";
 import {projectRef, fileRef} from "../types";
 import {appStateActions} from "../reducers/appStateReducer";
 import {userActions} from "../reducers/userReducer";
-import { Services } from "../helpers/Services";
-import { GenericError, LoginError  } from "../helpers/Errors";
+import { Services, GenericError, LoginError } from "../helpers/Services";
 import { showError } from "../partials/Errors";
 import { trim } from "ramda";
 import {settingsActions, settingsReducerState} from "../reducers/settingsReducer";
@@ -26,13 +25,11 @@ const mapDispatchToProps = (dispatch: Function) => {
 
     async function asyncAction<T>(pr: Promise<T>) {
         try {
-            const result = await pr;
-            return result;
+            return await pr;
         }catch (e) {
             if (e instanceof LoginError) {
                 showError(e.message);
                 dispatch({type: userActions.INVALIDATE});
-                throw null;
             } else {
                 throw e;
             }
@@ -43,37 +40,39 @@ const mapDispatchToProps = (dispatch: Function) => {
         dispatch: {
           settings: {
              updateSettings: (newSettings: settingsReducerState) => {
-                Services.storage().setSettings({id: 0, editor_mode: "standard",
+                asyncAction(Services.storage().setSettings({id: 0, editor_mode: "standard",
                 font_size: newSettings.fontSize,
                 font: newSettings.font,
                 theme: newSettings.theme ? "light" : "dark",
                 space_tab: true,
-                tab_width: newSettings.tabWidth}).then(() => dispatch({type: settingsActions.updateSettings, payload: newSettings}));
+                tab_width: newSettings.tabWidth})).then(() => dispatch({type: settingsActions.updateSettings, payload: newSettings}));
                 },
              updateEditorRatio: (ratio: number) => dispatch({type: settingsActions.updateEditorRatio, payload: ratio})
           },
-          // other than openFile and closeFile, the file name paramter should always be the full path, for exmaple "q1/file.txt"
+          // other than openFile and closeFile, the file name parameter should always be the full path, for exmaple "q1/file.txt"
           file: {
               copyFile: (targetName: string) => dispatch({type: appStateActions.copyFile, payload: {question: {name: "question", files: ["file1.txt"]}, newName: targetName.split("/").pop()}}),
               updateFile: (project: string, path: string, newFileContent: string) => {
-                  Services.storage().writeFile([project, path], newFileContent).then(() => dispatch({type: appStateActions.changeFileContent, payload: newFileContent})); },
-              addFile: (project: string, path: string, newFileContent: string) => {
-                  // writes a new file, then opens the question the new file was added to and opens the newly added file
-                  Services.storage().writeFile([project, path], newFileContent).then(()=>
-                  Services.storage().getFiles(project).then((files)=>dispatch({type: appStateActions.addFile, payload: {name: path.split("/")[0],
-                  files: files.filter((file)=>file.name.split("/")[0]===path.split("/")[0] || file.name.split("/")[0] === "tests" || file.name.split("/")[0] === "common").map((file) => file.name)},
-                  file: {name: path.split("/")[0], content: newFileContent}}))); },
+                  asyncAction(Services.storage().writeFile([project, path], newFileContent)).then(() => dispatch({type: appStateActions.changeFileContent, payload: newFileContent})); },
+              addFile: (project: string, path: string, newFileContent: string, currentQuestion: string) => {
+                  // writes a new file, returns a promise the caller can use when finished to do other stuff (i.e. switch to the file)
+                  return asyncAction(Services.storage().newFile(project, path, newFileContent)).then(()=>dispatch({type: appStateActions.addFile, payload: path})).catch((reason)=>{throw reason;});},
               deleteFile: (project: string, name: string) => {
-                  Services.storage().deleteFile([project, name]).then(dispatch({type: appStateActions.removeFile, payload: {name: name}})); },
+                  return asyncAction(Services.storage().deleteFile([project, name])).then(()=>{
+                      dispatch({type: appStateActions.closeFile, payload: name});
+                      dispatch({type: appStateActions.removeFile, payload: {name: name}});}).catch((reason)=>{throw reason;}); },
               renameFile: (project: string, currentname: string, targetName: string) => {
-                  Services.storage().renameFile([project, currentname], targetName).then(() => Services.storage().getFiles(project).then((files) => dispatch({type: appStateActions.renameFile, payload: {question: {name: targetName.split("/")[0],
-                  files: files.filter((file) => file.name.split("/")[0] === targetName.split("/")[0] || file.name.split("/")[0] === "tests" || file.name.split("/")[0] === "common").map((file) => file.name)},
-                  newName: targetName.split("/").pop()}}))); },
+                  return asyncAction(Services.storage().renameFile([project, currentname], targetName)).then(()=>{
+                      dispatch({type: appStateActions.closeFile, payload: name});
+                      dispatch({type: appStateActions.removeFile, payload: name});
+                      dispatch({type: appStateActions.addFile, payload: targetName});
+                      dispatch({type: appStateActions.openFile, payload: targetName});
+                  }).catch((reason)=>{throw reason;}); },
               openFile: (name: string) => dispatch({type: appStateActions.openFile, payload: name}),
               closeFile: (name: string) => dispatch({type: appStateActions.closeFile, payload: name}),
               setRunFile: (name: string) => dispatch({type: appStateActions.setRunFile, payload: name}),
               switchFile: (question: string, name: string) => {
-                    Services.storage().readFile([question, name]).then((file: File) => dispatch({type: appStateActions.switchFile, payload: {file: {name: name, content: file.contents}}}));
+                    return asyncAction(Services.storage().readFile([question, name])).then((file: File) => dispatch({type: appStateActions.switchFile, payload: {file: {name: name, content: file.contents}}}));
                 }
           },
           question: {
@@ -82,37 +81,22 @@ const mapDispatchToProps = (dispatch: Function) => {
               switchQuestion: (project: string, name: string) => {
                   // TODO interface with backend and get real data
                   // we will leave switching files to the UI
-                  Services.storage().getFiles(project).then((files) => dispatch({type: appStateActions.switchQuestion, payload: {question: {name: name, files: files.filter((file) => file.name.split("/")[0] === name).map((file) => file.name)}}}));
+                  return asyncAction(Services.storage().getFiles(project)).then((files) => dispatch({type: appStateActions.switchQuestion, payload: {question: {name: name, files: files.filter((file) => file.name.split("/")[0] === name).map((file) => file.name)}}}));
               }
           },
           user: {
             signin: (username: string, password: string) => {
-                return new Promise((resolve, reject) => {
-                    if (trim(username) === "" || trim(password) === "") {
-                        showError("Please fill in all fields!");
-                        resolve();
-                    } else {
-                        asyncAction(Services.login(username, password)).then((response) => {
-                            dispatch({type: userActions.SIGNIN, payload: username});
-                            reject(null);
-                        }).catch((reason) => {
-                            resolve();
-                            if (reason !== null) reject(reason);
-                        });
-                    }
-                });
-            },
-            signout: () => {
-                asyncAction(Services.logout()).then((response) => {
-                    dispatch({type: userActions.SIGNOUT});
-                }).catch((reason) => {
-                    if (reason !== null) throw reason;
-                });
+                if (trim(username) === "" || trim(password) === ""){
+                    showError("Please fill in all fields!");
+                } else {
+                    let result = asyncAction(Services.login(username, password));
+                    alert("hi!");
+                }
             }
           },
           project: {
-              addProject: (newProjectName: string) => Services.storage().newProject(newProjectName).then(() => dispatch({type: appStateActions.addProject, payload: {name: newProjectName}})),
-              removeProject: (name: string) => Services.storage().deleteProject(name).then(() => dispatch({type: appStateActions.removeProject, payload: {name: name}})),
+              addProject: (newProjectName: string) => asyncAction(Services.storage().newProject(newProjectName)).then(() => dispatch({type: appStateActions.addProject, payload: {name: newProjectName}})),
+              removeProject: (name: string) => asyncAction(Services.storage().deleteProject(name)).then(() => dispatch({type: appStateActions.removeProject, payload: {name: name}})),
               switchProject: (name: string) => {
                   // TODO interface with backend and get real data
                   // we will leave switching question and file to the UI
@@ -120,7 +104,7 @@ const mapDispatchToProps = (dispatch: Function) => {
                   function unique(val: any, idx: Number, arr: any){
                     return arr.indexOf(val) === idx;
                   }
-                  Services.storage().getFiles(name).then((files) => dispatch({type: appStateActions.switchProject, payload: {project: {name: name, questions: files.map((file) => file.name).filter(unique)}}}));
+                  return asyncAction(Services.storage().getFiles(name)).then((files) => dispatch({type: appStateActions.switchProject, payload: {project: {name: name, questions: files.map((file) => file.name).filter(unique)}}}));
               }
           },
           app: {
