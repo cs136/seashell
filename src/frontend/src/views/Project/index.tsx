6cec482dbb8393e90565077223eccdea533ac702
Kaleb Alway <kpalway@uwaterloo.ca>
2017-04-24 17:46:51 -0400

    Hook up tests again, pass project name properly to compiler

--- src/frontend/src/views/Project/index.tsx
+++ src/frontend/src/views/Project/index.tsx
@@ -75,7 +75,7 @@ class Project extends React.Component<ProjectProps&actionsInterface, ProjectStat
                             :
                             <button className="pt-button pt-minimal pt-icon-comparison"
                                 onClick={() =>
-                                    this.props.dispatch.file.flushFileBuffer().then(this.props.dispatch.compile.compileAndRun.bind(this, project.name, question.name, question.runFile, true))}>
+                                    this.props.dispatch.file.flushFileBuffer().then(this.props.dispatch.compile.compileAndRun.bind(this, project.id, question.name, question.runFile, true))}>
                             </button>}
                     </Tooltip>,
                     !question.runFile ?
