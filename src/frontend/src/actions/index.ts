ac464fadba4495679927709e684bfa9b7fd93d5e
Ryan Leung <rvleung@uwaterloo.ca>
2017-04-24 20:50:59 -0400

    more fixing up opening a project

--- src/frontend/src/actions/index.ts
+++ src/frontend/src/actions/index.ts
@@ -347,7 +347,7 @@ const mapDispatchToProps = (dispatch: Function) => {
                   termClear: null,
                   name: name,
                   id: pid,
-                  questions: files.map((file) => file.name.split("/")[0]).filter(unique),
+                  questions: files.map((file) => file.question()).filter(unique),
                   currentQuestion: undefined
                 }
               }
