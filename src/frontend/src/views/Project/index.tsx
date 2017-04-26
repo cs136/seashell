0ac18a4523b8c19e9c02efae86254b98768cbacb
Kevin Pei <kevin-pei@hotmail.com>
2017-04-26 02:27:58 -0400

    merge

--- src/frontend/src/views/Project/index.tsx
+++ src/frontend/src/views/Project/index.tsx
@@ -68,80 +68,6 @@ class Project extends React.Component<ProjectProps&actionsInterface, ProjectStat
   render() {
     const project = this.props.appState.currentProject;
     if (project) {
-<<<<<<< HEAD
-        const question = project.currentQuestion;
-        return (<div>
-            <Navigation navLeft={[
-                    <div className="pt-navbar-heading" key="project-name">{project.name}</div>,
-                    <Popover content={<QuestionList />} key="project-question" position={Position.BOTTOM}>
-                        <button className="pt-button pt-intent-primary"><span className="pt-icon-standard pt-icon-caret-down" />{question ? question.name : "Select a Question"}</button>
-                    </Popover>,
-                    question ? <span className="pt-navbar-divider" key="project-divider" /> : <span key="empty-project-divider" />,
-                    question ? <Popover content={<ListFiles question={question}/>} position={Position.BOTTOM} key="project-open-file">
-                        <button className="pt-button"><span className="pt-icon-standard pt-icon-caret-down" />Open File</button>
-                    </Popover> : <span key="empty-project-open-file"/>]}
-                navRight={question ? [
-                    <OpenFiles key="project-open-files" />,
-                    <Tooltip key="project-toggle-view" content="Toggle Editor/Console" position={Position.BOTTOM}>
-                        <button onClick={this.toggleView.bind(this)} className={"pt-button pt-minimal pt-icon-applications " + styles.toggleView}>
-                        </button>
-                    </Tooltip>,
-                    <Tooltip key="project-build-file" content="Test" position={Position.BOTTOM_RIGHT}>
-                        {this.props.appState.runState !== 0 || question.runFile === "" ?
-                            <button className="pt-button pt-minimal pt-disabled pt-icon-comparison"> </button>
-                            :
-                            <button className="pt-button pt-minimal pt-icon-comparison"
-                                onClick={() =>
-                                    this.props.dispatch.file.flushFileBuffer().then(this.props.dispatch.compile.compileAndRun.bind(this, project.id, question.name, question.runFile, true))}>
-                            </button>}
-                    </Tooltip>,
-                    !question.runFile ?
-                        <Tooltip key="project-run-file-set" content="Please set a run file" position={Position.BOTTOM_RIGHT}>
-                        <button className="pt-button pt-minimal pt-disabled pt-icon-play"></button></Tooltip>
-                    : this.props.appState.runState === 0 ?
-                        <Tooltip key="project-run-file" content="Run" position={Position.BOTTOM_RIGHT}>
-                        <button className="pt-button pt-minimal pt-icon-play"
-                            onClick={() => this.props.dispatch.file.flushFileBuffer().then(this.props.dispatch.compile.compileAndRun.bind(this, project.id, question.name, question.runFile, false))}>
-                        </button>
-                        </Tooltip>
-                        : this.props.appState.runState === 1 ?
-                            <Tooltip key="project-run-file" content="Compiling" position={Position.BOTTOM_RIGHT}>
-                                <button className="pt-button pt-minimal pt-disabled pt-icon-build">
-                            </button>
-                            </Tooltip>
-                            : <Tooltip key="project-run-file" content="Stop" position={Position.BOTTOM_RIGHT}>
-                                <button className="pt-button pt-minimal pt-icon-stop" onClick={() => this.props.dispatch.compile.stopProgram()}>
-                                </button>
-                                </Tooltip>, this.generateMarmosetButton(project)] : []}/>
-            {question && question.currentFile ?
-                <DisplayFiles className={this.state.toggleView ? styles.rightToggle : styles.leftToggle}
-                    file={question.currentFile.name} /> : <Splash />}
-            <Dialog className={styles.dialogStyle} title="Delete File"
-                isOpen={this.props.dialog.delete_file_open}
-                onClose={this.props.dispatch.dialog.toggleDeleteFile}>
-                    <DeletePrompt closefunc={this.props.dispatch.dialog.toggleDeleteFile}/>
-            </Dialog>
-            <Dialog className={styles.dialogStyle} title="Rename/Move File"
-                isOpen={this.props.dialog.rename_file_open}
-                onClose={this.props.dispatch.dialog.toggleRenameFile}>
-                <RenamePrompt questions={project.questions} closefunc={this.props.dispatch.dialog.toggleRenameFile}/>
-            </Dialog>
-            <Dialog className={styles.dialogStyle} title="Copy File"
-                isOpen={this.props.dialog.copy_file_open}
-                onClose={this.props.dispatch.dialog.toggleCopyFile}>
-                    <CopyPrompt questions={project.questions} closefunc={this.props.dispatch.dialog.toggleCopyFile}/>
-            </Dialog>
-            <Dialog className={styles.dialogStyle} title="Add File"
-                isOpen={this.props.dialog.add_file_open}
-                onClose={this.props.dispatch.dialog.toggleAddFile}>
-                    <AddFilePrompt questions={project.questions} closefunc={this.props.dispatch.dialog.toggleAddFile}/>
-            </Dialog>
-            <Dialog className={styles.dialogStyle} title="Marmoset Results"
-                isOpen={false}>
-                    <MarmosetResultPrompt />
-            </Dialog>
-        </div>);
-=======
       const question = project.currentQuestion;
       return (<div>
         <Navigation navLeft={[
@@ -239,8 +165,11 @@ class Project extends React.Component<ProjectProps&actionsInterface, ProjectStat
           <AddFilePrompt questions={project.questions}
               closefunc={this.props.dispatch.dialog.toggleAddFile}/>
         </Dialog>
+        <Dialog className={styles.dialogStyle} title="Marmoset Results"
+            isOpen={false}>
+                <MarmosetResultPrompt />
+        </Dialog>
       </div>);
->>>>>>> 98e8e709990cbf43998dc3e095737fab3064ecfd
     } else {
       return <Loading/>;
     }
