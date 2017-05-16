1c6eedbcea74224d899624cfd714bf0e6096eb87
Edward Lee <e45lee@uwaterloo.ca>
2017-05-15 20:37:54 -0400

    [backend] Split up and add SQLite bindings from #665; tests forthcoming.

---
+++ src/collects/seashell/db/database.rkt
@@ -0,0 +1,164 @@
+#lang typed/racket
+;; Seashell's SQLite3 + Dexie bindings.
+;; Copyright (C) 2013-2015 The Seashell Maintainers.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(require typed/json)
+(require typed/db)
+(require typed/db/sqlite3)
+(require "support.rkt")
+(require "changes.rkt")
+(require "updates.rkt")
+
+(provide sync-database%)
+
+(: true? (All (A) (-> (Option A) Any : #:+ A)))
+(define (true? x) x)
+
+(define sync-database%
+  (class object%
+    (init [path : SQLite3-Database-Storage])
+    (: database Connection)
+    (define database (sqlite-connection path))
+    (super-new)
+
+    (query-exec database "CREATE TABLE IF NOT EXISTS _clients (id TEXT PRIMARY KEY, description TEXT)")
+    (query-exec database "INSERT OR IGNORE INTO _clients VALUES ($1, $2)" SERVER_CLIENT_KEY "Server-side Writes")
+    (query-exec database "CREATE TABLE IF NOT EXISTS _changes (revision INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                               type INTEGER,
+                                                               client TEXT,
+                                                               target_table TEXT,
+                                                               target_key TEXT,
+                                                               data TEXT DEFAULT 'false',
+                                                               FOREIGN KEY(client) REFERENCES _clients(id))")
+    (query-exec database "CREATE TABLE IF NOT EXISTS _partials (client TEXT,
+                                                                type INTEGER,
+                                                                target_table TEXT,
+                                                                target_key text,
+                                                                data TEXT DEFAULT 'false',
+                                                                FOREIGN KEY(client) REFERENCES _clients(id))")
+    (: write-transaction (All (A) (-> (-> A) A)))
+    (define/public (write-transaction thunk)
+      (define option (if (db-in-transaction?) #f 'immediate))
+      (parameterize ([db-in-transaction? #t])
+        (call-with-transaction database thunk #:option option)))
+
+    (: read-transaction (All (A) (-> (-> A) A)))
+    (define/public (read-transaction thunk)
+      (define option (if (db-in-transaction?) #f 'deferred))
+      (parameterize ([db-in-transaction? #t])
+        (call-with-transaction database thunk #:option option)))
+
+    (: current-revision (-> Integer))
+    (define/public (current-revision)
+      (define result (query-value database "SELECT MAX(revision) FROM _changes"))
+      (assert result exact-integer?))
+
+    (: fetch (-> String String (Option JSExpr)))
+    (define/public (fetch table key)
+      (define result (query-maybe-value database (format "SELECT json(data) FROM ~a WHERE id=$1" table) key))
+      (if (string? result)
+          (string->jsexpr result)
+          #f))
+
+    (: apply-create (->* (String String (U String (HashTable Symbol JSExpr))) ((Option String) Boolean) Any))
+    (define/public (apply-create table key object [_client #f] [_transaction #t])
+      (define data (string-or-jsexpr->string object))
+      (define todo (thunk
+                    (query-exec database (format "CREATE TABLE IF NOT EXISTS '~a' (id TEXT PRIMARY KEY, data TEXT)" table))
+                    (query-exec database (format "INSERT OR REPLACE INTO '~a' (id, data) VALUES ($1, $2)" table) key data)
+                    (query-exec database "INSERT INTO _changes (client, type, target_table, target_key, data) VALUES ($1, $2, $3, $4, json($5))"
+                                (or _client SERVER_CLIENT_KEY)
+                                CREATE
+                                table
+                                key
+                                data)))
+      (if _transaction (write-transaction todo) (todo)))
+    (: apply-partial-create (->* (String String (U String (HashTable Symbol JSExpr))) ((Option String)) Any))
+    (define/public (apply-partial-create table key object [_client #f])
+      (define data (string-or-jsexpr->string object))
+      (query-exec database "INSERT INTO _partials (client, type, target_table, target_key, data) VALUES ($1, $2, $3, $4, json($5))"
+                  (or _client SERVER_CLIENT_KEY)
+                  CREATE
+                  table
+                  key
+                  data))
+
+    (: apply-update (->* (String String (HashTable Symbol JSExpr)) ((Option String) Boolean) Any))
+    (define/public (apply-update table key updates [_client #f] [_transaction #t])
+      (define data (string-or-jsexpr->string updates))
+      (define todo (thunk
+                    (define exists? (query-maybe-value database "SELECT 1 FROM sqlite_master WHERE type = $1 AND name = $2" "table" table))
+                    (when exists?
+                      (hash-for-each updates
+                                     (lambda ([_key : Symbol] [_value : JSExpr])
+                                       (define key-path (string-append "$." (symbol->string _key)))
+                                       (define update (jsexpr->string _value))
+                                       (query-exec database (format "UPDATE ~a SET data = json_set(json(data), $1, json($2)) WHERE id=$3" table) key-path update key)))
+                      (query-exec database "INSERT INTO _changes (client, type, target_table, target_key, data) VALUES ($1, $2, $3, $4, json($5))"
+                                  (or _client SERVER_CLIENT_KEY)
+                                  UPDATE
+                                  table
+                                  key
+                                  data))))
+      (if _transaction (write-transaction todo) (todo)))
+    (: apply-partial-update (->* (String String (HashTable Symbol JSExpr)) ((Option String)) Any))
+    (define/public (apply-partial-update table key updates [_client #f])
+      (define data (string-or-jsexpr->string updates))
+      (query-exec database "INSERT INTO _partials (client, type, target_table, target_key, data) VALUES ($1, $2, $3, $4, json($5))"
+                  (or _client SERVER_CLIENT_KEY)
+                  UPDATE
+                  table
+                  key
+                  data))
+ 
+    (: apply-delete (->* (String String) ((Option String) Boolean) Any))
+    (define/public (apply-delete table key [_client #f] [_transaction #t])
+      (define todo (thunk
+                    (define exists? (query-maybe-value database "SELECT 1 FROM sqlite_master WHERE type = $1 AND name = $2" "table" table))
+                    (when exists?
+                      (query-exec database (format "DELETE FROM ~a WHERE id = $1" table) key)
+                      (query-exec database "INSERT INTO _changes (client, type, target_table, target_key) VALUES ($1, $2, $3, $4)"
+                                  (or _client SERVER_CLIENT_KEY)
+                                  DELETE
+                                  table
+                                  key))))
+      (if _transaction (write-transaction todo) (todo)))
+    (: apply-partial-delete (->* (String String) ((Option String)) Any))
+    (define/public (apply-partial-delete table key [_client #f])
+      (query-exec database "INSERT INTO _partials (client, type, target_table, target_key) VALUES ($1, $2, $3, $4)"
+                  (or _client SERVER_CLIENT_KEY)
+                  DELETE
+                  table
+                  key))
+
+    (: apply-partials (->* () (Integer (Option String)) Any))
+    (define/public (apply-partials [base 0] [_client #f])
+      (define client (or _client SERVER_CLIENT_KEY))
+      (write-transaction
+       (thunk
+        (define changes (map row->change (query-rows database "SELECT type, client, target_table, target_key, json(data) FROM _partials WHERE client = $1" client)))
+        (define server-changes (map row->change (query-rows database "SELECT type, client, target_table, target_key, json(data) FROM _changes WHERE revision >= $1" base)))
+        (define resolved-changes (resolve-conflicts changes server-changes))
+        (display resolved-changes)
+        (for ([change resolved-changes])
+          (match-define (database-change type client table key data) change)
+          (cond
+            [(= type CREATE) (apply-create table key data client #f)]
+            [(= type UPDATE) (apply-update table key (assert (string->jsexpr data) hash?) client #f)]
+            [(= type DELETE) (apply-delete table key client #f)]))
+        (query-exec database "DELETE FROM _partials WHERE client = $1" client))))
+    ))
---
+++ src/collects/seashell/db/support.rkt
@@ -0,0 +1,99 @@
+#lang typed/racket
+;; Seashell's SQLite3 + Dexie bindings.
+;; Copyright (C) 2013-2015 The Seashell Maintainers.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(require typed/json)
+(require typed/db)
+(require typed/db/sqlite3)
+
+(provide string-or-jsexpr->string
+         jsexpr-or-string->jsexpr
+         set-by-key-path
+         delete-by-key-path
+         sqlite-connection
+         compute-conn
+         CREATE UPDATE DELETE
+         SERVER_CLIENT_KEY
+         SERVER_MERGE_KEY
+         db-in-transaction?)
+
+(define CREATE 1)
+(define UPDATE 2)
+(define DELETE 3)
+(define SERVER_CLIENT_KEY "__SERVER_CHANGE")
+(define SERVER_MERGE_KEY "__SERVER_MERGED_CHANGE")
+(: db-in-transaction? (Parameter Boolean))
+(define db-in-transaction? (make-parameter #f))
+
+;; (sqlite-connection path) -> Connection
+;; Opens a virtual connection to the specified SQLite3 database.
+;; Arguments:
+;;  path: Either a string or 'memory
+;; Returns:
+;;  A connection to the wanted SQLite3 database.
+(: sqlite-connection (-> SQLite3-Database-Storage Connection))
+(define (sqlite-connection path)
+  (virtual-connection
+   (connection-pool
+    (thunk (sqlite3-connect #:database path #:use-place #t)))))
+(define compute-conn (sqlite-connection 'memory))
+
+;; (string-or-jsexpr->string expr) -> String
+;; Converts a JSON expression or a String to a String.
+;;
+;; Arguments:
+;;  expr -> JSExpr | String
+(: string-or-jsexpr->string (-> (U String JSExpr) String))
+(define (string-or-jsexpr->string jsexpr)
+  (if (string? jsexpr) jsexpr (jsexpr->string jsexpr)))
+
+;; (string-or-jsexpr->string expr) -> String
+;; Converts a string or a JSON expression to a JSON expression.
+;;
+;; Arguments:
+;;  expr -> JSExpr | String
+(: jsexpr-or-string->jsexpr (-> (U String JSExpr) JSExpr))
+(define (jsexpr-or-string->jsexpr x)
+  (if (string? x) (string->jsexpr x) x))
+
+;; (set-by-key-path object key update) -> newObject (as String)
+;; Sets the specified key in object to the new value (specified in update)
+;; Returns a stringified representation of the resulting object.
+;;
+;; Arguments:
+;;  object - JSExpr | String representing the object to update.
+;;  key    - Key path to update.
+;;  update - New value to set.
+(: set-by-key-path (-> (U String JSExpr) String String String))
+(define (set-by-key-path object key update)
+  (assert (query-value compute-conn "SELECT json_set(json($1), $2, json($3))"
+                       (string-or-jsexpr->string object)
+                       (string-append "$." key)
+                       update) string?))
+
+;; (delete-by-key-path object key update) -> newObject (as String)
+;; Removes the specified key in object to the new value (specified in update)
+;; Returns a stringified representation of the resulting object.
+;;
+;; Arguments:
+;;  object - JSExpr | String representing the object to update.
+;;  key    - Key path to delete..
+(: delete-by-key-path (-> (U String JSExpr) String String))
+(define (delete-by-key-path object key)
+  (assert (query-value compute-conn "SELECT json_remove(json($1), $2)"
+                       (string-or-jsexpr->string object)
+                       (string-append "$." key)) string?))
