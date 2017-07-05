42a0f9d23f2cf71f31259a59b2c8d1972166a2dc
Kaleb Alway <kpalway@uwaterloo.ca>
2017-07-05 16:39:29 -0400

    Progress towards cleaning up the backend

--- src/collects/seashell/backend/db-files.rkt
+++ src/collects/seashell/backend/db-files.rkt
@@ -17,6 +17,7 @@
   [compile-and-run-project (->* (Path-String Path-String String (Listof String)) (Boolean) (Values Boolean (HashTable Symbol JSExpr)))])
 
 (provide new-project
+         restore-file-from-template
          delete-project
          new-file
          new-directory
@@ -53,6 +54,47 @@
                     (new-file pid (path->string p) (file->string p) 0)) (void))
                 (sequence->list (in-directory))))))))
     pid)))
+    
+
+;; (restore-file-from-template project file template)
+;; Restores a file from a skeleton/template.
+;;
+;; Args:
+;;  project - Project.
+;;  file - Path to file.
+;;  template - Path (possibly URL) to template.
+;; Returns:
+;;  MD5 hash of file.
+(: restore-file-from-template (-> String String String Void))
+(define (restore-file-from-template project file template)
+ (void))
+;(define/contract (restore-file-from-template project file template)
+;  (-> (and/c project-name? is-project?) path-string? (or/c path-string? url-string?) string?)
+;  (define ok #f)
+;  (define-values (question-dir filename _) (split-path file))
+;  (define dest-dir (check-and-build-path (build-project-path project) question-dir))
+;  (make-directory* dest-dir)
+;  (define destination (check-and-build-path dest-dir filename))
+;  (define source (explode-path file))
+;  (call-with-template template
+;                      (lambda (port)
+;                        (unzip port
+;                               (lambda (name _2 contents)
+;                                 (define lname (explode-path (simplify-path (bytes->path name) #f)))
+;                                 (when (and (not (null? lname))
+;                                            (equal? source (cdr lname)))
+;                                   (call-with-write-lock (thunk
+;                                     (call-with-output-file destination
+;                                                            (lambda (dport)
+;                                                              (define-values (md5in md5out) (make-pipe))
+;                                                              (copy-port contents dport md5out)
+;                                                              (close-output-port md5out)
+;                                                              (set! ok (md5 md5in)))
+;                                                            #:exists 'replace))))))))
+;  (when (not ok)
+;    (raise (exn:fail (format "File ~a (~a) not found in template ~a!" file source template)
+;           (current-continuation-marks))))
+;  ok)
 
 (: delete-project (-> String Void))
 (define (delete-project id)
