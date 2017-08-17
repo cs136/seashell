8cc5beeb14763db1b24235b9b8752f4f25b092b4
Kaleb Alway <kpalway@uwaterloo.ca>
2017-08-17 15:08:54 -0400

    More docs and a bugfix for skeletons

--- src/frontend/src/helpers/Storage/SkeletonManager.ts
+++ src/frontend/src/helpers/Storage/SkeletonManager.ts
@@ -7,6 +7,10 @@ import * as E from "../Errors";
 
 export {SkeletonManager};
 
+/* A project can either:
+   - not have a skeleton
+   - have a public skeleton
+   - have a whitelist skeleton */
 export enum SkeletonStatus { None, Public, Whitelist };
 
 const CS136_URL = "https://www.student.cs.uwaterloo.ca/~cs136/";
@@ -18,14 +22,25 @@ const SKEL_ROOT_URL = `${CS136_URL}assignment_skeletons/`;
 const WL_SKEL_ROOT_URL = "ssh://cs136@linux.student.cs.uwaterloo.ca:/u2/cs136/seashell-support-files/whitelist-skeletons/";
 const SKEL_FILE_LIST_URL = `${CGI_URL}skeleton_file_list.rkt`;
 
+/* SkeletonManager
+  Provides methods for dealing with Seashell skeletons (template projects) */
 class SkeletonManager {
 
+  /* SkeletonManager(socket, storage)
+    Args:
+     socket - the active SeashellWebsocket
+     storage - the active LocalStorage */
   constructor(private socket: SeashellWebsocket, private storage: LocalStorage) { }
 
   private userWhitelist: string[];
   private projectWhitelist: string[];
   private projectsWithSkeletons: string[];
 
+  /* getUserWhiteList()
+    Gets the list of "whitelisted" users - those who are able to download instructors-only skeleton projects.
+
+    Returns:
+     Array of whitelisted usernames */
   private async getUserWhitelist(): Promise<string[]> {
     if (!this.userWhitelist) {
       try {
@@ -47,12 +62,22 @@ class SkeletonManager {
     return this.userWhitelist || [];
   }
 
+  /* currentUserIsWhitelisted()
+    Determines if the current user is a whitelisted user.
+
+    Returns:
+     Boolean, true if user is whitelisted, false otherwise. */
   private async currentUserIsWhitelisted(): Promise<boolean> {
     const users = await this.getUserWhitelist();
     const current = this.socket.getUsername();
     return !!users.find((u: string) => u === current);
   }
 
+  /* getProjectsWithWhitelistSkeletons()
+    Gets the list of projects that have a whitelist-only skeleton.
+
+    Returns:
+     Array of project names */
   private async getProjectsWithWhitelistSkeletons(): Promise<string[]> {
     if (!this.projectWhitelist) {
       try {
@@ -72,6 +97,11 @@ class SkeletonManager {
     return this.projectWhitelist || [];
   }
 
+  /* getProjectsWithSkeletons()
+    Gets the list of (non-whitelist) skeleton projects.
+
+    Returns:
+     Array of project names */
   private async getProjectsWithSkeletons(): Promise<string[]> {
     if (!this.projectsWithSkeletons) {
       try {
@@ -92,6 +122,13 @@ class SkeletonManager {
     return this.projectsWithSkeletons || [];
   }
 
+  /* _inSkeleton(pname)
+    Determines the SkeletonStatus of a project.
+
+    Args:
+     pname - project name
+    Returns:
+     The SkeletonStatus of that project */
   private async _inSkeleton(pname: string): Promise<SkeletonStatus> {
     let [names, users, wlnames] =
       await Promise.all([this.getProjectsWithSkeletons(),
@@ -106,20 +143,35 @@ class SkeletonManager {
     return ans;
   }
 
-  public async inSkeleton(proj: ProjectID): Promise<boolean> {
-    const stat = await this._inSkeleton(proj);
+  /* inSkeleton(proj)
+    Determines whether a project has an associated skeleton.
+
+    Args:
+     pid - project ID
+    Returns:
+     True if project has a skeleton, false otherwise */
+  public async inSkeleton(pid: ProjectID): Promise<boolean> {
+    const proj = await this.storage.getProject(pid);
+    const stat = await this._inSkeleton(proj.name);
     return stat !== SkeletonStatus.None;
   }
 
-  private async listSkeletonFiles(proj: ProjectID): Promise<string[]> {
-    const project = await this.storage.getProject(proj);
+  /* listSkeletonFiles(pid)
+    Gets a list of files in the skeleton of the given project.
+
+    Args:
+     pid - project ID
+    Returns:
+     Array of filenames in the skeleton */
+  private async listSkeletonFiles(pid: ProjectID): Promise<string[]> {
+    const project = await this.storage.getProject(pid);
     try {
       const zipURL = await this.getSkeletonZipFileURL(project.name);
       if (zipURL) {
         let querybuilder = new URLSearchParams();
         querybuilder.append("template", project.name);
         querybuilder.append("user", this.socket.getUsername());
-        querybuilder.append("whitelist", (await this._inSkeleton(proj)) === SkeletonStatus.Whitelist ? "true" : "false");
+        querybuilder.append("whitelist", (await this._inSkeleton(pid)) === SkeletonStatus.Whitelist ? "true" : "false");
         let query = querybuilder.toString();
         let raw = await fetch(`${SKEL_FILE_LIST_URL}?${query}`);
         if (raw.ok) {
@@ -146,13 +198,28 @@ class SkeletonManager {
     }
   }
 
-  private async getMissingSkeletonFiles(proj: ProjectID): Promise<string[]> {
+  /* getMissingSkeletonFiles(pid)
+    Gets a list of files that are present in the skeleton for a project, but missing
+    in the user's copy of the project.
+
+    Args:
+     pid - project ID
+    Returns:
+     Array of filenames */
+  private async getMissingSkeletonFiles(pid: ProjectID): Promise<string[]> {
     let [localFileObjList, serverFileList] =
-      await Promise.all([this.storage.getFiles(proj), this.listSkeletonFiles(proj)]);
+      await Promise.all([this.storage.getFiles(pid), this.listSkeletonFiles(pid)]);
     let localFileList = localFileObjList.map((f: File) => f.name);
     return serverFileList.filter((f: string) => !localFileList.find((g: string) => f === g));
   }
 
+  /* getSkeletonZipFileURL(pname)
+    Gets the URL of the .zip file for a project's skeleton.
+
+    Args:
+     pname - project name
+    Returns:
+     URL as a string, or false if there is no associated skeleton */
   private async getSkeletonZipFileURL(pname: string): Promise<string|false> {
     const stat = await this._inSkeleton(pname);
     if (stat === SkeletonStatus.Public) {
@@ -164,9 +231,15 @@ class SkeletonManager {
     }
   }
 
-  public async pullMissingSkeletonFiles(proj: ProjectID): Promise<void> {
-    const project = await this.storage.getProject(proj);
-    let missingFiles = await this.getMissingSkeletonFiles(proj);
+  /* pullMissingSkeletonFiles(pid)
+    Sends websocket requests to copy all files that are present in the skeleton
+    but missing in the user's copy of the project.
+
+    Args:
+     pid - project ID */
+  public async pullMissingSkeletonFiles(pid: ProjectID): Promise<void> {
+    const project = await this.storage.getProject(pid);
+    let missingFiles = await this.getMissingSkeletonFiles(pid);
     if (missingFiles.length > 0) {
       await Promise.all(missingFiles.map(async (f: string) =>
         this.socket.sendMessage({
@@ -186,8 +259,12 @@ class SkeletonManager {
     }
   }
 
-  /* Promise resolves to a list of the new projects that were cloned
-      from skeletons. */
+  /* fetchNewSkeletons()
+    Sends websocket requests to clone any skeletons which do not already
+    exist in the user's projects.
+
+    Returns:
+     Array of project names that were newly cloned. */
   public async fetchNewSkeletons(): Promise<string[]> {
     const localProjects = (await this.storage.getProjects())
       .map((p: Project) => p.name);
