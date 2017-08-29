2fafbe779fca4d46d74f95ac9f20da40891c6f99
Edward Lee <e45lee@uwaterloo.ca>
2017-08-29 08:37:11 -0400

    [frontend] Add test for checkCredentials.

--- src/frontend/test/Storage/Crypto.test.ts
+++ src/frontend/test/Storage/Crypto.test.ts
@@ -1,4 +1,6 @@
-import {Coder} from "../../src/helpers/Crypto";
+import {storeCredentials, checkCredentials, Coder} from "../../src/helpers/Crypto";
+import * as LS from "localstorage-memory";
+(<any>window).localStorage = LS;
 
 describe("Testing crypto.ts", () => {
   it("Coder.encrypt", async () => {
@@ -11,5 +13,12 @@ describe("Testing crypto.ts", () => {
     expect(result.iv).toEqual([254, 255, 122, 107, 246, 190, 147, 191, 239, 255, 255, 188]);
     expect(result.encrypted).toEqual([212, 8, 245, 234, 124, 24, 4, 112, 55, 91, 133, 99, 43, 136, 31, 111, 207, 39, 32, 153, 157, 66, 28, 238, 102, 90, 39, 135, 8, 101, 160, 71, 73, 151, 192, 147, 121, 36, 201, 132, 177, 212, 67, 107, 28, 126, 190, 150, 184, 221, 249, 253, 134, 201, 132, 143, 50, 4, 102, 107, 20, 67, 167, 129]);
     expect(result.authTag).toEqual([211, 24, 56, 236, 206, 253, 197, 99, 206, 35, 231, 227, 158, 224, 57, 122]);
+  });
+
+  it("checkCredentials", async () => {
+    await storeCredentials("foo", "bar");
+    expect(checkCredentials("foo", "bar")).resolves.toBe(expect.anything());
+    expect(checkCredentials("foo", "bar2")).rejects.toBe(expect.anything());
+    expect(checkCredentials("foo2", "bar")).rejects.toBe(expect.anything());
   });
 });
